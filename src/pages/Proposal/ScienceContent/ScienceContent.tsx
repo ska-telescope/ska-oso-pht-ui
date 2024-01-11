2a40d98c6d2ce1c62d50b02e6d12c49dbceaf52b
gallacherc <chloe.gallacher@cgi.com>
2024-01-11 11:08:12 +0000

    STAR-106: WIP, refactoring science page to enable pdf file upload

--- src/pages/Proposal/ScienceContent/ScienceContent.tsx
+++ src/pages/Proposal/ScienceContent/ScienceContent.tsx
@@ -1,28 +1,13 @@
 import React, {useState} from 'react';
-import {
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  Grid,
-  IconButton,
-  Modal,
-  Typography
-} from '@mui/material';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import UploadPdfButton from '../../../components/button/uploadPdf/UploadPdfButton';
 
 import {STATUS_ERROR, STATUS_OK, STATUS_PARTIAL } from '../../../utils/constants';
-import PDFUpload from "../../../components/pdfUpload/pdfUpload";
 
 interface ScienceContentProps {
   page: number;
 }
 
 export default function ScienceContent({ page}: ScienceContentProps) {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [pdfUrl] = React.useState('');
-
   const [file, setFile] = useState<File | null>(null);
   const [status, setStatus] = useState<
       "initial" | "uploading" | "success" | "fail"
@@ -43,14 +28,6 @@ export default function ScienceContent({ page}: ScienceContentProps) {
 
     setStatus([page, result[count]]);
   }, [setStatus]);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-  function closeModal() {
-    setIsOpen(false);
-  }
-
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setStatus("initial");
@@ -93,62 +70,6 @@ export default function ScienceContent({ page}: ScienceContentProps) {
       return null;
     }
   };
-
-  const pdfUploadModal = () => (
-    <Modal open={isOpen}>
-      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-        <Card variant="outlined" sx={{ height: '90vh', width: '90vw' }}>
-          <CardHeader
-            action={(
-              <IconButton
-                aria-label="DUMMY"
-                sx={{ '&:hover': { backgroundColor: 'primary.dark' }, ml: 1 }}
-                onClick={() => closeModal()}
-                color="inherit"
-              >
-                <HighlightOffIcon />
-              </IconButton>
-            )}
-            title={<Typography variant="h6">PDF Preview</Typography>}
-          />
-          <CardContent sx={{ height: '90vh', width: '90vw' }}>
-            <object data={pdfUrl} type="application/pdf" width="100%" height="100%">
-              <p>Syntax error or PDF not available </p>
-            </object>
-          </CardContent>
-        </Card>
-      </Box>
-    </Modal>
-  );
-
-  // return (
-  //   <>
-  //     {pdfUploadModal()}
-  //     <Grid container direction="column" alignItems="space-evenly" justifyContent="space-around">
-  //       <Grid item>
-  //         <Grid container p={5} spacing={5} direction="row" justifyContent="space-evenly">
-  //           <Grid item xs={6}>
-  //             <Grid container direction="column" alignItems="left">
-  //               <Typography variant="h5">Upload PDF</Typography>
-  //               <PDFUpload
-  //                 // eslint-disable-next-line react/jsx-no-bind
-  //                 setModal={openModal}
-  //               />
-  //               <Grid container direction="row" justifyContent="space-between">
-  //                 <UploadPdfButton />
-  //               </Grid>
-  //             </Grid>
-  //           </Grid>
-  //           <Grid item xs={6}>
-  //             <Box m={1}>
-  //               <Typography variant="h5">Uploaded PDF Preview</Typography>
-  //             </Box>
-  //           </Grid>
-  //         </Grid>
-  //       </Grid>
-  //     </Grid>
-  //   </>
-  // );
 
   return (
       <>
