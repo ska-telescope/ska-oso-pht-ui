434b47252e3dfe0a54f42812fdea300184b7f030
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-01-21 22:08:05 +0000

    STAR-112 - correct upload url

--- src/pages/Proposal/ScienceContent/ScienceContent.tsx
+++ src/pages/Proposal/ScienceContent/ScienceContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
 import { FileUpload, FileUploadStatus } from '@ska-telescope/ska-gui-components';
 import { Proposal } from '../../../services/types/proposal';
-import { STATUS_ERROR, STATUS_OK, STATUS_PARTIAL, SKA_PHT_UPLOAD_API_URL, SKA_PHT_API_URL } from '../../../utils/constants';
+import { STATUS_ERROR, STATUS_OK, STATUS_PARTIAL, SKA_PHT_UPLOAD_API_URL } from '../../../utils/constants';
 
 interface ScienceContentProps {
   page: number;
@@ -61,7 +61,7 @@ export default function ScienceContent({
           maxFileWidth={25}
           setFile={setFile}
           setStatus={setUploadStatus}
-          uploadURL={SKA_PHT_API_URL}
+          uploadURL={SKA_PHT_UPLOAD_API_URL}
         />
       </Grid>
       <Grid item xs={6}>
