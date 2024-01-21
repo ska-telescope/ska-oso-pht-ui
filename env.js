b55082a791733c2acfe3f8cb458b0ec11f152499
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-01-21 22:04:18 +0000

    STAR-112 - connect POST upload/pdf endpoint

--- env.js
+++ env.js
@@ -3,5 +3,6 @@ export const env = {
   ...window.env,
   ...(typeof Cypress !== 'undefined' ? Cypress.env() : {}),
   REACT_APP_SKA_PHT_API_URL: 'http://127.0.0.1:5000/ska-oso-pht-services/pht/api/v1/proposal',
+  REACT_APP_SKA_PHT_UPLOAD_API_URL: 'http://127.0.0.1:5000/ska-oso-pht-services/pht/api/v1/upload/pdf',
   REACT_APP_USE_LOCAL_DATA: false
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 import { env } from '../../env';
 
 export const SKA_PHT_API_URL = env.REACT_APP_SKA_PHT_API_URL;
+export const SKA_PHT_UPLOAD_API_URL = env.REACT_APP_SKA_PHT_UPLOAD_API_URL;
 export const USE_LOCAL_DATA = env.REACT_APP_USE_LOCAL_DATA;
 
 export const STATUS_OK = 0;
