82bd29348c36c32102482b1083642d5bdd015b93
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-01-23 09:17:29 +0000

    STAR-112 - add TODO for unit tests

---
+++ src/components/layout/pageBanner/PageBanner.cy.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-restricted-syntax */
+import React from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { THEME_DARK, THEME_LIGHT } from '@ska-telescope/ska-gui-components';
+import { Router } from 'react-router-dom';
+import theme from '../../services/theme/theme';
+import PageBanner from './PageBanner';
+
+const THEME = [THEME_DARK, THEME_LIGHT];
+
+describe('<AddProposal />', () => {
+  for (const theTheme of THEME) {
+    it(`Theme ${theTheme}: Renders`, () => {
+      cy.mount(
+        <ThemeProvider theme={theme(theTheme)}>
+          <CssBaseline />
+          <PageBanner title={''} />
+        </ThemeProvider>
+      );
+    });
+  }
+});
+
+describe('POST proposal/ bad request', () => {
+  beforeEach(() => {
+    // cy.intercept('POST', `${SKA_PHT_API_URL}`, { statusCode: 500 }).as('postProposalFail');
+    cy.mount(
+      <Router location="/" navigator={undefined}>
+        <PageBanner title={''} />
+      </Router>
+    );
+  });
+
+// TODO: create unit tests for SAVE and VALIDATE buttons
