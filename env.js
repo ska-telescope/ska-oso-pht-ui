418699a410afb51c119e366b316c2e0f450e199a
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-01-31 09:41:43 +0000

    STAR-117 undo URL change

--- env.js
+++ env.js
@@ -2,8 +2,7 @@ export const env = {
   //    ...process.env,
   ...window.env,
   ...(typeof Cypress !== 'undefined' ? Cypress.env() : {}),
-  REACT_APP_SKA_PHT_API_URL: '/dp-naledi-dominic/pht/api/v1',
-  // REACT_APP_SKA_PHT_API_URL: 'http://localhost:5000', // production url ?
+  REACT_APP_SKA_PHT_API_URL: 'http://localhost:5000', // production url ?
   // REACT_APP_SKA_PHT_API_URL: 'http://127.0.0.1:5000/ska-oso-pht-services/pht/api/v1', // local dev url - to not push
-  REACT_APP_USE_LOCAL_DATA: false
+  REACT_APP_USE_LOCAL_DATA: true
 };
