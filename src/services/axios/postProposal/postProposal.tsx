5cf523807ead88644d2947164e725ed967c5078e
t.swain <trevor.swain@cgi.com>
2024-02-13 13:33:25 +0000

    STAR-197 : Moved id into useState

--- src/services/axios/postProposal/postProposal.tsx
+++ src/services/axios/postProposal/postProposal.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { helpers } from '../../../utils/helpers';
-import { SKA_PHT_API_URL, USE_LOCAL_DATA } from '../../../utils/constants';
+// import { SKA_PHT_API_URL, USE_LOCAL_DATA } from '../../../utils/constants';
 import { Proposal } from '../../types/proposal';
 
 async function PostProposal(proposal: Proposal, status?) {
   // const apiUrl = SKA_PHT_API_URL;
-  const apiUrl = 'http://192.168.49.2/ska-oso-pht-services/pht/api/v1'
+  const apiUrl = 'http://192.168.49.2/ska-oso-pht-services/pht/api/v1';
   const URL_NEW = `/proposals`;
   const config = {
     headers: {
