8dc8d645ddd128d9d975ea46febbc1323dafc8d8
Chloe <chloe.gallacher@cgi.com>
2024-02-21 15:31:20 +0000

    STAR-208: Updated Loader.cy.tsx

--- src/components/layout/Loader/Loader.cy.tsx
+++ src/components/layout/Loader/Loader.cy.tsx
@@ -4,6 +4,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { THEME_DARK, THEME_LIGHT } from '@ska-telescope/ska-gui-components';
 import theme from '../../../services/theme/theme';
 import Loader from './Loader';
+import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 
 const THEME = [THEME_DARK, THEME_LIGHT];
 
@@ -11,10 +12,12 @@ describe('<Legend />', () => {
   for (const theTheme of THEME) {
     it(`Theme ${theTheme}: Renders`, () => {
       cy.mount(
-        <ThemeProvider theme={theme(theTheme)}>
-          <CssBaseline />
-          <Loader />
-        </ThemeProvider>
+        <StoreProvider>
+          <ThemeProvider theme={theme(theTheme)}>
+            <CssBaseline />
+            <Loader />
+          </ThemeProvider>
+        </StoreProvider>
       );
     });
   }
