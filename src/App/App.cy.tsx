245fb9938d66172304b048ea3044d46a92490368
Chloe <chloe.gallacher@cgi.com>
2024-02-21 14:39:58 +0000

    STAR-208: Updated App.cy.tsx

--- src/App/App.cy.tsx
+++ src/App/App.cy.tsx
@@ -4,6 +4,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { THEME_DARK, THEME_LIGHT } from '@ska-telescope/ska-gui-components';
 import theme from '../services/theme/theme';
 import App from './App';
+import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 
 const THEME = [THEME_DARK, THEME_LIGHT];
 
@@ -11,11 +12,13 @@ describe('<PHT />', () => {
   for (const theTheme of THEME) {
     it(`Theme ${theTheme}: Renders`, () => {
       cy.mount(
+        <StoreProvider>
         <ThemeProvider theme={theme(theTheme)}>
           <CssBaseline />
           <App />
         </ThemeProvider>
-      );
+          </StoreProvider>
+          );
     });
   }
 });
