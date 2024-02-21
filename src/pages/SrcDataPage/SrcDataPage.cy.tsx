156bca3af77c579264ebe19f80293b8867cfa9b6
Chloe <chloe.gallacher@cgi.com>
2024-02-21 17:24:24 +0000

    STAR-208: Updated SrcDataPage.cy.tsx

--- src/pages/SrcDataPage/SrcDataPage.cy.tsx
+++ src/pages/SrcDataPage/SrcDataPage.cy.tsx
@@ -4,6 +4,8 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { THEME_DARK, THEME_LIGHT } from '@ska-telescope/ska-gui-components';
 import theme from '../../services/theme/theme';
 import SrcDataPage from './SrcDataPage';
+import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
+import { Router } from 'react-router-dom';
 
 const THEME = [THEME_DARK, THEME_LIGHT];
 
@@ -11,11 +13,15 @@ describe('<DataPage />', () => {
   for (const theTheme of THEME) {
     it(`Theme ${theTheme}: Renders`, () => {
       cy.mount(
+        <StoreProvider>
         <ThemeProvider theme={theme(theTheme)}>
           <CssBaseline />
+          <Router location="/" navigator={undefined}>
           <SrcDataPage />
+            </Router>
         </ThemeProvider>
-      );
+          </StoreProvider>
+          );
     });
   }
 });
