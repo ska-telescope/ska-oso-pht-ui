e6387ba4ca0fd613ef573e56ee02b3e2a2f319c0
Chloe <chloe.gallacher@cgi.com>
2024-02-22 11:12:49 +0000

    STAR-208: Updated TitlePage.cy.tsx

--- src/pages/TitlePage/TitlePage.cy.tsx
+++ src/pages/TitlePage/TitlePage.cy.tsx
@@ -4,6 +4,9 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { THEME_DARK, THEME_LIGHT } from '@ska-telescope/ska-gui-components';
 import theme from '../../services/theme/theme';
 import TitlePage from './TitlePage';
+import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
+import MemberInvite from '../TeamPage/MemberInvite/MemberInvite';
+import { Router } from 'react-router-dom';
 
 const THEME = [THEME_DARK, THEME_LIGHT];
 
@@ -11,10 +14,14 @@ describe('<Proposal />', () => {
   for (const theTheme of THEME) {
     it(`Theme ${theTheme}: Renders`, () => {
       cy.mount(
-        <ThemeProvider theme={theme(theTheme)}>
+      <StoreProvider>
+        <ThemeProvider theme={theme(THEME_LIGHT)}>
           <CssBaseline />
+          <Router location="/" navigator={undefined}>
           <TitlePage />
+            </Router>
         </ThemeProvider>
+      </StoreProvider>
       );
     });
   }
