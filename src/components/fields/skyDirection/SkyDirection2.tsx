6aa0d85225aab5e5d3b7c1677074e345d0e25c13
Chloe <chloe.gallacher@cgi.com>
2024-03-29 13:01:56 +0000

    STAR-315: Tidy up validation added

--- src/components/fields/skyDirection/SkyDirection2.tsx
+++ src/components/fields/skyDirection/SkyDirection2.tsx
@@ -10,7 +10,6 @@ interface SkyDirection2FieldProps {
   value: string;
   valueFocus?: Function;
   valueTypeFocus?: Function;
-  required?: boolean;
 }
 
 export default function SkyDirection2Field({
