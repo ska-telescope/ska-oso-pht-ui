411cf3f51f414b7fd9a80734343985dae479e53e
t.swain <trevor.swain@cgi.com>
2024-04-09 14:07:37 +0100

    STAR-405 : Missed a save

--- src/services/axios/putProposal/putProposal.tsx
+++ src/services/axios/putProposal/putProposal.tsx
@@ -1,26 +1,21 @@
 import axios from 'axios';
-import { SKA_PHT_API_URL, USE_LOCAL_DATA } from '../../../utils/constants';
+import { AXIOS_CONFIG, SKA_PHT_API_URL, USE_LOCAL_DATA } from '../../../utils/constants';
 import { helpers } from '../../../utils/helpers';
 
 async function PutProposal(proposal, status?) {
-  const apiUrl = SKA_PHT_API_URL;
-  const URL_EDIT = `/proposals/${proposal.id}`;
-  const config = {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  };
-
   if (USE_LOCAL_DATA) {
     return 'success';
   }
 
-  // TODO: add testing for proposal conversion format
-  const convertedProposal = helpers.transform.convertProposalToBackendFormat(proposal, status);
-
   try {
-    const result = await axios.put(`${apiUrl}${URL_EDIT}`, convertedProposal, config);
+    const URL_PATH = `/proposals/${proposal.id}`;
+    // TODO: add testing for proposal conversion format
+    const convertedProposal = helpers.transform.convertProposalToBackendFormat(proposal, status);
+    const result = await axios.put(
+      `${SKA_PHT_API_URL}${URL_PATH}`,
+      convertedProposal,
+      AXIOS_CONFIG
+    );
     return typeof result === 'undefined' ? 'error.API_UNKNOWN_ERROR' : result.data;
   } catch (e) {
     return { error: e.message };
