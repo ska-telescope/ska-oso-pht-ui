5dc8a10298fd4dee8d957e07202326bc77852a54
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-04-16 15:15:49 +0100

    STAR-306 prepare display of results in table

--- src/components/sensCalcDisplay/single/SensCalcDisplaySingle.tsx
+++ src/components/sensCalcDisplay/single/SensCalcDisplaySingle.tsx
@@ -3,7 +3,7 @@ import { StatusIcon } from '@ska-telescope/ska-gui-components';
 import getSensCalc, {
   SENSCALC_EMPTY
 } from '../../../services/axios/sensitivityCalculator/getSensitivityCalculatorAPIData';
-import { IconButton } from '@mui/material';
+import { Grid, IconButton, Typography } from '@mui/material';
 import SensCalcModalSingle from '../../alerts/sensCalcModal/single/SensCalcModalSingle';
 import Observation from '../../../utils/types/observation';
 import Target from '../../../utils/types/target';
@@ -34,9 +34,19 @@ export default function SensCalcDisplaySingle({
 
   return (
     <>
-      <IconButton aria-label="SensCalc Status" style={{ cursor: 'hand' }} onClick={IconClicked}>
-        <StatusIcon ariaTitle="" testId="statusId" icon level={results.status} size={SIZE} />
-      </IconButton>
+      <Grid container direction="row" justifyContent="flex-start" alignItems="center">
+        <Grid mr={10}>
+          <IconButton aria-label="SensCalc Status" style={{ cursor: 'hand' }} onClick={IconClicked}>
+            <StatusIcon ariaTitle="" testId="statusId" icon level={results.status} size={SIZE} />
+          </IconButton>
+        </Grid>
+        <Grid mr={10}>
+          <Typography>TEST 1{/*results?.totalSensitivity?.value*/}</Typography>
+        </Grid>
+        <Grid>
+          <Typography>TEST 2{/*results?.integrationTime?.value*/}</Typography>
+        </Grid>
+      </Grid>
       <SensCalcModalSingle open={openDialog} onClose={() => setOpenDialog(false)} data={results} />
     </>
   );
