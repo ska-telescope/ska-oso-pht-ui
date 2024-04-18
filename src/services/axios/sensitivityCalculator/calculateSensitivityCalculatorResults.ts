3f5b453d87eb79ba1a3c6fef5e865db539065441
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-04-18 09:16:43 +0100

    STAR-306 code cleaning

--- src/services/axios/sensitivityCalculator/calculateSensitivityCalculatorResults.ts
+++ src/services/axios/sensitivityCalculator/calculateSensitivityCalculatorResults.ts
@@ -1,5 +1,4 @@
 import sensCalHelpers from './sensCalHelpers';
-// import SensitivityCalculatorResults from './../../../utils/types/sensitivityCalculatorResults';
 import {
   SensitivityCalculatorAPIResponseLow,
   SensitivityCalculatorAPIResponseMid
@@ -12,22 +11,63 @@ let confusionNoise: number;
 let weightedSensitivity: number;
 let totalSensitivity: number;
 let totalSensitivityDisplayValue: any;
-let integrationTimeDisplayValue: string;
 
 export default function calculateSensitivityCalculatorResults(
   // response: SensitivityCalculatorAPIResponseLow | SensitivityCalculatorAPIResponseMid,
   response: any,
   observation: Observation
-): SensCalcResult /*: SensitivityCalculatorResults*/ {
-  //  TODO: change type to SensCalcResult like in getSensitivityCalculatorAPIData and remove SensitivityCalculatorResults type
+): SensCalcResult {
   confusionNoise = getConfusionNoise(response);
   weightedSensitivity = getWeightedSensitivity(response);
   totalSensitivity = getSensitivity(confusionNoise, weightedSensitivity);
   totalSensitivityDisplayValue = sensCalHelpers.format.convertSensitivityToDisplayValue(
     totalSensitivity
   );
-  integrationTimeDisplayValue = `${observation.integration_time} 
-  ${sensCalHelpers.format.getIntegrationTimeUnitsLabel(observation.integration_time_units)}`;
+  /*
+  return {
+    title: 'TITLE FIELD',
+    status: STATUS_OK,
+    // if continuum (use getCalculate and getWeighting) // name without saying continuum or spectral?
+    section1: [
+      { field: 'continuumSensitivityWeighted', value: '84.47', units: 'ujy/beam (6.10)' },
+      { field: 'continuumConfusionNoise', value: '3.63', units: 'mjy/beam' },
+      {
+        field: 'continuumTotalSensitivity',
+        value: totalSensitivityDisplayValue?.value,
+        units: totalSensitivityDisplayValue?.units
+      },
+      { field: 'continuumSynthBeamSize', value: '190.0" x 171.3"', units: '' },
+      { field: 'continuumSurfaceBrightnessSensitivity', value: '3.40', units: 'k' }
+    ],
+    // if continuum (use getCalculate and getWeightingLine) // same as section1?
+    section2: [
+      { field: 'spectralSensitivityWeighted', value: '(2.62)', units: '' },
+      { field: 'spectralConfusionNoise', value: '6.02', units: 'mjy/beam' },
+      { field: 'spectralTotalSensitivity', value: '9.45', units: 'mjy/beam' },
+      { field: 'spectralSynthBeamSize', value: '230.0" x 207.8"', units: '' },
+      { field: 'spectralSurfaceBrightnessSensitivity', value: '6.04', units: 'k' }
+    ],
+    // if zoom (use getCalculate and getWeighting) // call line? or spectral?
+    section3: [
+      { field: 'spectralSensitivityWeighted', value: '(2.62)', units: '' },
+      { field: 'spectralConfusionNoise', value: '6.02', units: 'mjy/beam' },
+      { field: 'spectralTotalSensitivity', value: '9.45', units: 'mjy/beam' },
+      { field: 'spectralSynthBeamSize', value: '230.0" x 207.8"', units: '' },
+      { field: 'spectralSurfaceBrightnessSensitivity', value: '6.04', units: 'k' }
+    ],
+    // ?
+    section4: [
+      {
+        field: 'continuumIntegrationTime',
+        value: observation.integration_time,
+        units: sensCalHelpers.format.getIntegrationTimeUnitsLabel(
+          observation.integration_time_units
+        )
+      },
+      { field: 'spectralIntegrationTime', value: '36.0', units: 'rad/m squared' }
+    ]
+  } as SensCalcResult;
+  */
   return {
     title: 'TITLE FIELD',
     status: STATUS_OK,
@@ -60,12 +100,6 @@ export default function calculateSensitivityCalculatorResults(
       { field: 'spectralIntegrationTime', value: '36.0', units: 'rad/m squared' }
     ]
   } as SensCalcResult;
-  /*
-  return {
-    totalSensitivity: { value: totalSensitivityDisplayValue },
-    integrationTime: { value: integrationTimeDisplayValue }
-  };
-  */
 }
 
 function getConfusionNoise(
