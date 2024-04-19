1f646a32efbb6b4286c1cbf81bcd3602d9e9478e
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-04-19 11:58:00 +0100

    STAR-306 - update snesCalcModalSingle tests

--- src/components/alerts/sensCalcModal/single/SensCalcModalSingle.cy.tsx
+++ src/components/alerts/sensCalcModal/single/SensCalcModalSingle.cy.tsx
@@ -7,54 +7,12 @@ import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 import theme from '../../../../services/theme/theme';
 import SensCalcDisplaySingle from './SensCalcModalSingle';
 import {
-  SENSCALC_EMPTY,
-  SENSCALC_MOCKED
-} from '../../../../services/axios/sensitivityCalculator/getSensitivityCalculatorAPIData';
+  SENSCALC_EMPTY_MOCKED,
+  SENSCALC_CONTINUUM_MOCKED,
+  SENSCALC_SPECTRAL_MOCKED
+} from '../../../../services/axios/sensitivityCalculator/SensCalcResultsMOCK';
 
 const THEME = [THEME_DARK, THEME_LIGHT];
-
-// LOW API response
-/*
-const response = {
-  calculate: {
-    sensitivity: 5.4349377028499,
-    units: 'uJy/beam'
-  },
-  weighting: {
-    weighting_factor: 15.176737500353465,
-    sbs_conv_factor: [2598892.1330005163],
-    confusion_noise: {
-      value: [0.00000103538377754085],
-      limit_type: ['value']
-    },
-    beam_size: [
-      {
-        beam_maj_scaled: 0.0010792161880542248,
-        beam_min_scaled: 0.0008405281736775013,
-        beam_pa: 190.60129457149282
-      }
-    ]
-  },
-  weightingLine: {
-    weighting_factor: 12.696037725198487,
-    sbs_conv_factor: [1035304.4396926606],
-    confusion_noise: {
-      value: [0.000003549738945339684],
-      limit_type: ['value']
-    },
-    beam_size: [
-      {
-        beam_maj_scaled: 0.0016339516370391025,
-        beam_min_scaled: 0.0013936114519620306,
-        beam_pa: 17.878165434661216
-      }
-    ]
-  }
-};
-const lvlError = 1;
-const lvlOk = 0;
-const observation = { telescope: 2 };
-*/
 
 describe('<ObservationTargetResultsDisplay />', () => {
   for (const theTheme of THEME) {
@@ -67,7 +25,7 @@ describe('<ObservationTargetResultsDisplay />', () => {
             <SensCalcDisplaySingle
               open
               onClose={cy.stub().as('handleCancel')}
-              data={SENSCALC_EMPTY}
+              data={SENSCALC_EMPTY_MOCKED}
             />
           </ThemeProvider>
         </StoreProvider>
@@ -82,7 +40,11 @@ describe('Modal with no data', () => {
     cy.mount(
       <StoreProvider>
         <Router location="/" navigator={undefined}>
-          <SensCalcDisplaySingle open onClose={cy.stub().as('handleClose')} data={SENSCALC_EMPTY} />
+          <SensCalcDisplaySingle
+            open
+            onClose={cy.stub().as('handleClose')}
+            data={SENSCALC_EMPTY_MOCKED}
+          />
         </Router>
       </StoreProvider>
     );
@@ -95,7 +57,7 @@ describe('Modal with no data', () => {
   });
 });
 
-describe('Modal with data', () => {
+describe('Modal with data - Continuum', () => {
   beforeEach(() => {
     cy.viewport(1500, 1500);
     cy.mount(
@@ -104,7 +66,7 @@ describe('Modal with data', () => {
           <SensCalcDisplaySingle
             open
             onClose={cy.stub().as('handleClose')}
-            data={SENSCALC_MOCKED}
+            data={SENSCALC_CONTINUUM_MOCKED}
           />
         </Router>
       </StoreProvider>
@@ -116,7 +78,7 @@ describe('Modal with data', () => {
   it('Ok icon should be displayed when data', () => {
     cy.get('[aria-label="Status Indicator 0"]').should('be.visible');
   });
-  it('Alert should display appropriate results', () => {
+  it('Alert should display appropriate results (continuum)', () => {
     cy.get('[id="continuumSensitivityWeighted"]').should(
       'contain',
       'sensitivityCalculatorResults.continuumSensitivityWeighted'
@@ -137,7 +99,55 @@ describe('Modal with data', () => {
       'contain',
       'sensitivityCalculatorResults.continuumSurfaceBrightnessSensitivity'
     );
+    cy.get('[id="continuumSpectralLineSensitivityWeighted"]').should(
+      'contain',
+      'sensitivityCalculatorResults.continuumSpectralLineSensitivityWeighted'
+    );
+    cy.get('[id="continuumSpectralLineConfusionNoise"]').should(
+      'contain',
+      'sensitivityCalculatorResults.continuumSpectralLineConfusionNoise'
+    );
+    cy.get('[id="continuumSpectralLineTotalSensitivity"]').should(
+      'contain',
+      'sensitivityCalculatorResults.continuumSpectralLineTotalSensitivity'
+    );
+    cy.get('[id="continuumSpectralLineSynthBeamSize"]').should(
+      'contain',
+      'sensitivityCalculatorResults.continuumSpectralLineSynthBeamSize'
+    );
+    cy.get('[id="continuumSpectralLineSurfaceBrightnessSensitivity"]').should(
+      'contain',
+      'sensitivityCalculatorResults.continuumSpectralLineSurfaceBrightnessSensitivity'
+    );
+    cy.get('[id="integrationTime"]').should(
+      'contain',
+      'sensitivityCalculatorResults.integrationTime'
+    );
+  });
+});
 
+describe('Modal with data - Spectral', () => {
+  beforeEach(() => {
+    cy.viewport(1500, 1500);
+    cy.mount(
+      <StoreProvider>
+        <Router location="/" navigator={undefined}>
+          <SensCalcDisplaySingle
+            open
+            onClose={cy.stub().as('handleClose')}
+            data={SENSCALC_SPECTRAL_MOCKED}
+          />
+        </Router>
+      </StoreProvider>
+    );
+  });
+  it('Alert should not be displayed when data', () => {
+    cy.get('[data-testid="alertSensCalResultsId"]').should('not.exist');
+  });
+  it('Ok icon should be displayed when data', () => {
+    cy.get('[aria-label="Status Indicator 0"]').should('be.visible');
+  });
+  it('Alert should display appropriate results (spectral)', () => {
     cy.get('[id="spectralSensitivityWeighted"]').should(
       'contain',
       'sensitivityCalculatorResults.spectralSensitivityWeighted'
@@ -157,6 +167,10 @@ describe('Modal with data', () => {
     cy.get('[id="spectralSurfaceBrightnessSensitivity"]').should(
       'contain',
       'sensitivityCalculatorResults.spectralSurfaceBrightnessSensitivity'
+    );
+    cy.get('[id="integrationTime"]').should(
+      'contain',
+      'sensitivityCalculatorResults.integrationTime'
     );
   });
 });
