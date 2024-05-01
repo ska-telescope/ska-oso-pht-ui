3ea0862bf3cc2044d7aae501c8dafeae8ce39b16
t.swain <trevor.swain@cgi.com>
2024-05-01 21:10:22 +0100

    STAR-404 : Altered port to 6101 & disabled tests

--- public/env.js
+++ public/env.js
@@ -4,7 +4,7 @@ window.env = {
   "REACT_APP_SKA_SENSITIVITY_CALC_URL_LOCAL": "http://192.168.49.2/ska-ost-senscalc-ui/api_doc/",
   "REACT_APP_SKA_SENSITIVITY_CALC_URL": "https://sensitivity-calculator.skao.int/api/v9/",
   "REACT_APP_USE_LOCAL_DATA_SENSITIVITY_CALC": "false",
-  "REACT_APP_USE_LOCAL_DATA": "true",
+  "REACT_APP_USE_LOCAL_DATA": "false",
   "REACT_APP_DOMAIN": "https://sdhp.stfc.skao.int",
   "REACT_APP_SKA_LOGIN_APP_URL": "http://localhost:4201"
 }
