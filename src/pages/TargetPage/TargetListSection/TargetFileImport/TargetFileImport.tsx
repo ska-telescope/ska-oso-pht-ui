91e2e25b7d4e15e2947c34014b7179494a17f09c
Jack Tam <jack.tam@akkodis.com>
2024-05-23 16:23:52 +0100

    STAR-165: fix lint

--- src/pages/TargetPage/TargetListSection/TargetFileImport/TargetFileImport.tsx
+++ src/pages/TargetPage/TargetListSection/TargetFileImport/TargetFileImport.tsx
@@ -12,8 +12,7 @@ export default function TargetFileImport({ raType }: TargetFileImportProps) {
   const [uploadButtonStatus, setUploadButtonStatus] = React.useState<FileUploadStatus>(null);
 
   const setFile = (theFile: File) => {
-
-    console.log('theFile', theFile)
+    console.log('theFile', theFile);
     //TODO: to decide when to set sciencePDF when adding the link in PUT endpoint
     //setProposal({ ...getProposal(), sciencePDF: theFile });
   };
@@ -24,17 +23,13 @@ export default function TargetFileImport({ raType }: TargetFileImportProps) {
   // };
 
   const uploadPdftoSignedUrl = async theFile => {
-   // setUploadStatus(FileUploadStatus.PENDING);
-
+    // setUploadStatus(FileUploadStatus.PENDING);
     // try {
     //   const proposal = getProposal();
     //   const prsl_id = proposal.id;
     //   const signedUrl = await GetPresignedUploadUrl(`${prsl_id}-science.pdf`);
-
     //   if (typeof signedUrl != 'string') new Error('Not able to Get Science PDF Upload URL');
-
     //   const uploadResult = await PutUploadPDF(signedUrl, theFile);
-
     //   if (uploadResult.error) {
     //     throw new Error('Science PDF Not Uploaded');
     //   }
@@ -50,18 +45,18 @@ export default function TargetFileImport({ raType }: TargetFileImportProps) {
       <Typography>{raType}</Typography>
 
       <FileUpload
-            chooseFileTypes=".csv"
-            clearLabel={t('clearBtn.label')}
-            clearToolTip={t('clearBtn.toolTip')}
-            direction="column"
-            //file={getProposal()?.sciencePDF}
-            maxFileWidth={25}
-            setFile={setFile}
-            //setStatus={setUploadStatus}
-            testId="fileUpload"
-            uploadFunction={uploadPdftoSignedUrl}
-            //status={uploadButtonStatus}
-          />
+        chooseFileTypes=".csv"
+        clearLabel={t('clearBtn.label')}
+        clearToolTip={t('clearBtn.toolTip')}
+        direction="column"
+        //file={getProposal()?.sciencePDF}
+        maxFileWidth={25}
+        setFile={setFile}
+        //setStatus={setUploadStatus}
+        testId="fileUpload"
+        uploadFunction={uploadPdftoSignedUrl}
+        //status={uploadButtonStatus}
+      />
     </>
   );
 }
