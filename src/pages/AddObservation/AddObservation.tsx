3cc3f21c5bd23ac8e3f3ba6b09cdf0e9cedd6f26
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-05-24 11:28:29 +0100

    STAR-511 lint fix

--- src/pages/AddObservation/AddObservation.tsx
+++ src/pages/AddObservation/AddObservation.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import {
-  Box,
-  Card,
-  CardContent,
-  Grid,
-  InputLabel,
-  Paper,
-  Typography
-} from '@mui/material';
+import { Box, Card, CardContent, Grid, InputLabel, Paper, Typography } from '@mui/material';
 import { storageObject } from '@ska-telescope/ska-gui-local-storage';
 import {
   ButtonColorTypes,
