037b8ba54e9f185c62f21582f17676b6a2a44d1c
Trevor Swain <trevor.swain@cgi.com>
2024-06-03 10:37:56 +0100

    STAR-318 : PDFViewer update

--- src/components/layout/PDFViewer/PDFViewer.tsx
+++ src/components/layout/PDFViewer/PDFViewer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card, CardContent, CardHeader, Grid } from '@mui/material';
+import { Card, Grid, Typography } from '@mui/material';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
@@ -29,54 +29,65 @@ export default function PDFViewer({ file }: PDFViewerProps) {
 
   const goToNextPage = () => setPageNumber(pageNumber + 1 >= numPages ? numPages : pageNumber + 1);
 
+  const displayTitle = () => {
+    return <Typography variant="h5">{file ? file : t('pdfPreview.label')}</Typography>;
+  };
+
+  const displayNavigation = () => {
+    return (
+      <Grid
+        spacing={1}
+        container
+        direction="row"
+        alignItems="space-evenly"
+        justifyContent="space-between"
+      >
+        <Grid item>
+          {numPages > 1 && (
+            <PreviousPageButton
+              disabled={pageNumber === 1}
+              label={t('page.previous')}
+              action={goToPrevPage}
+            />
+          )}
+        </Grid>
+        <Grid item>
+          {numPages > 1 ? t('page.pageOf', { current: pageNumber, max: numPages }) : ''}
+        </Grid>
+        <Grid item>
+          {numPages > 1 && (
+            <NextPageButton
+              disabled={pageNumber === numPages}
+              label={t('page.next')}
+              action={goToNextPage}
+            />
+          )}
+        </Grid>
+      </Grid>
+    );
+  };
+
   return (
     <Card>
-      <CardHeader
-        title={file ? file : t('pdfPreview.label')}
-        titleTypographyProps={{
-          align: 'center',
-          fontWeight: 'bold',
-          variant: 'h5'
-        }}
-        subheader={
-          <Grid
-            spacing={1}
-            container
-            direction="row"
-            alignItems="space-evenly"
-            justifyContent="space-between"
-          >
-            <Grid item>
-              {numPages > 1 && (
-                <PreviousPageButton
-                  disabled={pageNumber === 1}
-                  label={t('page.previous')}
-                  action={goToPrevPage}
-                />
-              )}
-            </Grid>
-            <Grid>
-              {numPages > 1 ? t('page.pageOf', { current: pageNumber, max: numPages }) : ''}
-            </Grid>
-            <Grid>
-              {numPages > 1 && (
-                <NextPageButton
-                  disabled={pageNumber === numPages}
-                  label={t('page.next')}
-                  action={goToNextPage}
-                />
-              )}
-            </Grid>
-          </Grid>
-        }
-      />
-      <CardContent>
-        {file?.length > 0 && (
-          <Document file={LOCAL_PREFIX + file} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
-          </Document>
-        )}
-      </CardContent>
+      <Grid
+        spacing={1}
+        container
+        direction="row"
+        alignItems="space-evenly"
+        justifyContent="space-around"
+      >
+        <Grid item>{displayTitle()}</Grid>
+        <Grid item xs={8}>
+          {displayNavigation()}
+        </Grid>
+        <Grid item>
+          {file?.length > 0 && (
+            <Document file={LOCAL_PREFIX + file} onLoadSuccess={onDocumentLoadSuccess}>
+              <Page pageNumber={pageNumber} />
+            </Document>
+          )}
+        </Grid>
+      </Grid>
     </Card>
   );
 }
