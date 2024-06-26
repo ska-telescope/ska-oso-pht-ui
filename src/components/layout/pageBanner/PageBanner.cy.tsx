90d2fcfaaabb93bde317e0c519354492d304e154
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-06-26 16:18:52 +0100

    STAR-529 PageBanner tests refactor

--- src/components/layout/pageBanner/PageBanner.cy.tsx
+++ src/components/layout/pageBanner/PageBanner.cy.tsx
@@ -28,7 +28,7 @@ describe('<AddProposal />', () => {
 
 describe('POST proposal/ bad request', () => {
   beforeEach(() => {
-    // cy.intercept('POST', `${SKA_PHT_API_URL}`, { statusCode: 500 }).as('postProposalFail');
+    // cy.intercept('POST', `{$SKA_PHT_API_URL}`, { statusCode: 500 }).as('postProposalFail');
     cy.mount(
       <StoreProvider>
         <Router location="/" navigator={undefined}>
@@ -49,8 +49,22 @@ describe('PUT proposal (SAVE)', () => {
       </StoreProvider>
     );
   });
-  it('displays request message in Alert component on request, Request Failed', () => {
+  it('displays request message in Alert component on save request, Request Failed', () => {
+    cy.intercept('PUT', `/__cypress/iframes/undefined/proposals/undefined`, {
+      statusCode: 500,
+      body: 'Internal Server Error'
+    }).as('apiCallSave500');
     cy.get('[data-testid="saveButtonTestId"]').click();
+    cy.wait('@apiCallSave500');
+    // TODO: Add verification of popup which lasts a few seconds
+  });
+  it('displays request message in Alert component on save request, Request OK', () => {
+    cy.intercept('PUT', `/__cypress/iframes/undefined/proposals/undefined`, {
+      statusCode: 200,
+      body: 'OK'
+    }).as('apiCallSaveOK');
+    cy.get('[data-testid="saveButtonTestId"]').click();
+    cy.wait('@apiCallSaveOK');
     // TODO: Add verification of popup which lasts a few seconds
   });
 });
@@ -65,23 +79,23 @@ describe('POST proposal (VALIDATE)', () => {
       </StoreProvider>
     );
   });
-  it('displays validate error message in Alert component on request, Request Failed', () => {
-    cy.intercept('POST', 'http://localhost:6101/__cypress/iframes/undefined/proposals/validate', {
+  it('displays validate error message in Alert component on validate request, Request Failed', () => {
+    cy.intercept('POST', `/__cypress/iframes/undefined/proposals/validate`, {
       statusCode: 500,
       body: 'Internal Server Error'
-    }).as('apiCall500');
+    }).as('apiCallValidate500');
     cy.get('[data-testid="validationBtn.labelButton"]').click();
-    cy.wait('@apiCall500');
+    cy.wait('@apiCallValidate500');
     // TODO: Add verification of popup which lasts a few seconds
     // cy.contains('Internal Server Error')
   });
-  it('displays validate confirmation message in Alert component on request, Request Success', () => {
-    cy.intercept('POST', 'http://localhost:6101/__cypress/iframes/undefined/proposals/validate', {
+  it('displays validate confirmation message in Alert component on validate request, Request Success', () => {
+    cy.intercept('POST', '/__cypress/iframes/undefined/proposals/validate', {
       statusCode: 200,
       body: 'OK'
-    }).as('apiCallOK');
+    }).as('apiCallValidateOK');
     cy.get('[data-testid="validationBtn.labelButton"]').click();
-    cy.wait('@apiCallOK');
+    cy.wait('@apiCallValidateOK');
     // TODO: Add verification of popup which lasts a few seconds
   });
 });
