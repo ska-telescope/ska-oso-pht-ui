04f3e411213f4f35618d7ba19e5c1cf0b61a55e1
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-07-02 11:17:23 +0100

    STAR-508 - lint fix

--- src/pages/AddObservation/AddObservation.tsx
+++ src/pages/AddObservation/AddObservation.tsx
@@ -1023,7 +1023,10 @@ export default function AddObservation() {
       const resolution = Number(spectralResolutionValue[0]);
       const centralFrequency = getScaledValue(frequency, 1000000, '*');
       const decimal = observationType === 1 ? 2 : 1;
-      const velocity = observationType === 1 ? calculateVelocity(resolution * spectralAveraging * 1000, centralFrequency) : calculateVelocity(resolution * spectralAveraging, centralFrequency);
+      const velocity =
+        observationType === 1
+          ? calculateVelocity(resolution * spectralAveraging * 1000, centralFrequency)
+          : calculateVelocity(resolution * spectralAveraging, centralFrequency);
       return `${effectiveResolution.toFixed(decimal)} ${unit} (${velocity})`;
     };
 
