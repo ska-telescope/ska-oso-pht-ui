d8f460e788640dcc99d2443d23631ce682bb3e19
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-07-11 12:25:40 +0100

    STAR-606 - change elevation type until new pdm changes are merged

--- src/utils/types/observationSet.tsx
+++ src/utils/types/observationSet.tsx
@@ -6,7 +6,8 @@ export type ObservationSetBackend = {
   observation_set_id: string;
   group_id?: string;
   observing_band?: string;
-  elevation?: ElevationBackend;
+  // elevation?: ElevationBackend; // TODO: use this once latest PDM changes merged
+  elevation?: number;
   array_details?: ArrayDetailsLowBackend | ArrayDetailsMidBackend;
   observation_type_details?: ObservationTypeDetailsBackend;
   details: string;
