580683fd72402dde1da94f09f4adaa03f36ffaae
Chloe <chloe.gallacher@cgi.com>
2024-08-12 21:07:55 +0100

    STAR-620: Added new e2e test for editing a proposal, added common steps

---
+++ cypress/integration/common/common.js
@@ -0,0 +1,125 @@
+export const clickAddProposal = () => {
+  cy.get('[data-testid="addProposalButton"]').should('exist');
+  cy.get('[data-testid="addProposalButton"]').click();
+  cy.get('[id="titleId"]').should('be.visible');
+};
+
+export const clickStandardProposalSubTypeTargetOfOpportunity = () => {
+  cy.get('[id="titleId"]').type("Proposal Title");
+  cy.get('[id="ProposalType-1"]').click({ force: true });
+  cy.get('[aria-label="A target of opportunity observing proposal"]').click();
+};
+
+export const clickCreateProposal = () => {
+  cy.get('[data-testid="CreateButton"]').click();
+  cy.get('[data-testid="timeAlertFooter"]').should('exist');
+  pageConfirmed('TEAM');
+};
+
+export const clickEditProposal = () => {
+  cy.get('[data-testid="EditRoundedIcon"]').click();
+  pageConfirmed('TITLE');
+};
+
+export const pageConfirmed = label => {
+  cy.get('#pageTitle').contains(label);
+};
+
+export const clickSaveProposal = () => {
+  cy.get('[data-testid="saveButtonTestId"]').should('exist');
+  cy.get('[data-testid="saveButtonTestId"]').click();
+};
+
+export const clickHome = () => {
+  cy.get('[data-testid="homeButtonTestId"]').should('exist');
+  cy.get('[data-testid="homeButtonTestId"]').click();
+};
+
+export const clickToTeamPage = () => {
+  cy.get('[data-testid="TeamButton"]').should('exist');
+  cy.get('[data-testid="TeamButton"]').click();
+};
+
+export const clickToGeneralPage = () => {
+  cy.get('[data-testid="GeneralButton"]').should('exist');
+  cy.get('[data-testid="GeneralButton"]').click();
+};
+
+export const clickToSciencePage = () => {
+  cy.get('[data-testid="ScienceButton"]').should('exist');
+  cy.get('[data-testid="ScienceButton"]').click();
+};
+
+export const clickToTargetPage = () => {
+  cy.get('[data-testid="TargetButton"]').should('exist');
+  cy.get('[data-testid="TargetButton"]').click();
+};
+
+export const clickToObservationPage = () => {
+  cy.get('[data-testid="ObservationButton"]').should('exist');
+  cy.get('[data-testid="ObservationButton"]').click();
+};
+
+export const clickToTechnicalPage = () => {
+  cy.get('[data-testid="TechnicalButton"]').should('exist');
+  cy.get('[data-testid="TechnicalButton"]').click();
+};
+
+export const clickToSDPDataPage = () => {
+  cy.get('[data-testid="SDP DataButton"]').should('exist');
+  cy.get('[data-testid="SDP DataButton"]').click();
+};
+
+export const addAbstract = () => {
+  cy.get('[id="abstractId"]').should('exist');
+  cy.get('[id="abstractId"]').type("Test abstract");
+};
+
+export const addTargetUsingResolve = () => {
+  cy.get('[id="name"]').should('exist');
+  cy.get('[id="name"]').type("M1");
+  cy.get('[data-testid="resolveButton"]').click();
+};
+
+export const clickToAddTarget = () => {
+  cy.get('[data-testid="addTargetButton"]').click();
+};
+
+export const clickObservationSetup = () => {
+  cy.get('[data-testid="addObservationButton"]').click();
+}
+
+export const clickAddObservation = () => {
+  cy.get('[data-testid="addObservationButton"]').click();
+}
+
+export const verifyProposalOnLandingPage = () => {
+  cy.get('div[role="presentation"].MuiDataGrid-virtualScrollerContent > div[role="rowgroup"]')
+    .children('div[role="row"]')
+    .should('contain', 'prsl-t0001-')
+    .should('contain', 'Cosmology')
+    .should('contain', 'test')
+    .should('have.length', 1);
+};
+
+export const verifyObservationInTable = () => {
+  cy.get('div[role="presentation"].MuiDataGrid-virtualScrollerContent > div[role="rowgroup"]')
+    .children('div[role="row"]')
+    .should('contain', 'obs-')
+    .should('contain', 'AA4')
+    .should('have.length', 2);
+};
+
+export const clickObservationFromTable = () => {
+  cy.get('[data-rowindex="0"]').click({ multiple: true });
+};
+export const clickToLinkTargetAndObservation = () => {
+  cy.get('[data-testid="linkedTickBox"]').click();
+};
+
+export const clickToValidateProposal = () => {
+  cy.get('[data-testid="ValidationTestId"]').should('exist');
+  cy.get('[data-testid="ValidationTestId"]').click();
+};
+
+
---
+++ cypress/integration/editProposal/editProposal.js
@@ -0,0 +1,68 @@
+import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
+import {
+  addAbstract,
+  addTargetUsingResolve,
+  clickAddObservation,
+  clickAddProposal,
+  clickCreateProposal,
+  clickEditProposal,
+  clickHome,
+  clickObservationFromTable,
+  clickObservationSetup,
+  clickSaveProposal,
+  clickStandardProposalSubTypeTargetOfOpportunity,
+  clickToAddTarget,
+  clickToGeneralPage,
+  clickToLinkTargetAndObservation,
+  clickToObservationPage,
+  clickToSciencePage,
+  clickToSDPDataPage,
+  clickToTargetPage,
+  clickToTeamPage,
+  clickToTechnicalPage, clickToValidateProposal,
+  pageConfirmed,
+  verifyObservationInTable,
+  verifyProposalOnLandingPage
+} from '../common/common';
+
+Given('I am a PHT user who wants to continue editing my previously created proposal', () => {
+  clickAddProposal()
+  clickStandardProposalSubTypeTargetOfOpportunity()
+  clickCreateProposal()
+  pageConfirmed('TEAM');
+  clickSaveProposal()
+});
+
+When('I get on the landing page and click on the edit button', () => {
+  clickHome()
+  verifyProposalOnLandingPage()
+  clickEditProposal()
+});
+
+Then('I am able to continue my proposal from where I stopped, fill in all the necessary details', () => {
+  clickToTeamPage()
+  clickToGeneralPage()
+  addAbstract()
+  clickToSciencePage()
+  clickToTargetPage()
+  addTargetUsingResolve()
+  clickToAddTarget()
+  clickToObservationPage()
+  clickObservationSetup()
+  clickAddObservation()
+  verifyObservationInTable()
+  clickObservationFromTable()
+  clickToLinkTargetAndObservation()
+  clickToTechnicalPage()
+  clickToSDPDataPage()
+});
+
+And('I validate my proposal', () => {
+  clickToValidateProposal()
+});
+
+And('I submit my proposal', () => {
+});
+
+And('the proposal status should change to submitted', () => {
+});
