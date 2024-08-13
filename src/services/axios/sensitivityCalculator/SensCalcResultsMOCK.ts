6ad7505413cbb91e5878c8e5fa5595775fce1d87
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-08-13 21:49:22 +0100

    STAR-615 -test- correct spelling of arsecs units into arsec so its accepted by backend

--- src/services/axios/sensitivityCalculator/SensCalcResultsMOCK.ts
+++ src/services/axios/sensitivityCalculator/SensCalcResultsMOCK.ts
@@ -19,14 +19,14 @@ export const SENSCALC_CONTINUUM_MOCKED: SensCalcResults = {
     { field: 'continuumSensitivityWeighted', value: '84.47', units: 'ujy/beam' },
     { field: 'continuumConfusionNoise', value: '3.63', units: 'mjy/beam' },
     { field: 'continuumTotalSensitivity', value: '3.64', units: 'mjy/beam' },
-    { field: 'continuumSynthBeamSize', value: '190.0 x 171.3', units: 'arcsecs2' },
+    { field: 'continuumSynthBeamSize', value: '190.0 x 171.3', units: 'arcsec2' },
     { field: 'continuumSurfaceBrightnessSensitivity', value: '3.40', units: 'k' }
   ],
   section2: [
     { field: 'spectralSensitivityWeighted', value: '2.62', units: 'ujy/beam' },
     { field: 'spectralConfusionNoise', value: '6.02', units: 'mjy/beam' },
     { field: 'spectralTotalSensitivity', value: '9.45', units: 'mjy/beam' },
-    { field: 'spectralSynthBeamSize', value: '230.0 x 207.8', units: 'arcsecs2' },
+    { field: 'spectralSynthBeamSize', value: '230.0 x 207.8', units: 'arcsec2' },
     { field: 'spectralSurfaceBrightnessSensitivity', value: '6.04', units: 'k' }
   ],
   section3: [{ field: 'integrationTime', value: '19.3', units: 'hours' }]
@@ -41,7 +41,7 @@ export const SENSCALC_SPECTRAL_MOCKED: SensCalcResults = {
     { field: 'spectralSensitivityWeighted', value: '84.47', units: 'ujy/beam' },
     { field: 'spectralConfusionNoise', value: '3.63', units: 'mjy/beam' },
     { field: 'spectralTotalSensitivity', value: '3.64', units: 'mjy/beam' },
-    { field: 'spectralSynthBeamSize', value: '190.0 x 171.3', units: 'arcsecs2' },
+    { field: 'spectralSynthBeamSize', value: '190.0 x 171.3', units: 'arcsec2' },
     { field: 'spectralSurfaceBrightnessSensitivity', value: '3.40', units: 'k' }
   ],
   section3: [{ field: 'integrationTime', value: '19.3', units: 'hours' }]
