8be33ba63d175b002b32aa6754753b08cd2e6243
Chloe <chloe.gallacher@cgi.com>
2024-08-14 13:21:20 +0100

    STAR-620: Refactor verification of proposal after creation

--- cypress/integration/common/common.js
+++ cypress/integration/common/common.js
@@ -117,8 +117,7 @@ export const verifyOnLandingPage = () => {
 };
 
 export const verifyProposalOnLandingPage = () => {
-  cy.reload()
-  cy.get('[data-testid="dataGridId"]').should('exist')
+  // cy.get('[data-testid="dataGridId"]').should('exist')
 };
 
 export const verifyObservationInTable = () => {
