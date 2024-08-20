da35228ee1ac286c01ca3cd775dd1263b8efe5bc
Chloe <chloe.gallacher@cgi.com>
2024-08-20 15:45:32 +0100

    STAR-620: Refactor putProposal

--- src/services/axios/putProposal/putProposal.tsx
+++ src/services/axios/putProposal/putProposal.tsx
@@ -3,9 +3,6 @@ import { AXIOS_CONFIG, SKA_PHT_API_URL, USE_LOCAL_DATA } from '../../../utils/co
 import MappingPutProposal from './putProposalMapping';
 
 async function PutProposal(proposal, status?) {
-  if (window.Cypress || USE_LOCAL_DATA) {
-    return 'success';
-  }
 
   try {
     const URL_PATH = `/proposals/${proposal.id}`;
