02900dc26650b345b661b39a454132ecdb033f67
Ugur Yilmaz <ugur.yilmaz@skao.int>
2024-09-30 13:16:48 +0100

    ST-2180: add base url to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
     excludeSpecPattern: 'tests/cypress/e2e/**'
   },
   e2e: {
+    baseUrl: 'http://localhost:8090',
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber());
