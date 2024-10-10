c3180d32b55b526b7880262ffc0bbbdd75998be6
Chloe <chloe.gallacher@cgi.com>
2024-10-10 15:25:34 +0100

    STAR-509: Add e2e tests for zoom spectral average range checks

--- cypress/integration/observations/observations.js
+++ cypress/integration/observations/observations.js
@@ -31,7 +31,10 @@ Then('I verify spectral average limits for LOW Continuum observations', () => {
   verifyContinuumSpectralAverageRangeAA1();
 });
 
-And('I verify spectral average limits for LOW Zoom observations', () => {});
+And('I verify spectral average limits for LOW Zoom observations', () => {
+  verifyZoomSpectralAverageRangeAA2Core();
+  verifyZoomSpectralAverageRangeCustom();
+});
 
 const verifyContinuumSpectralAverageRangeAA4 = value => {
   enterSpectralAverageValue(0);
@@ -55,6 +58,23 @@ const verifyContinuumSpectralAverageRangeAA1 = value => {
   enterSpectralAverageValue(6907);
   verifySpectralAverageRangeError();
 };
+
+const verifyZoomSpectralAverageRangeAA2Core = value => {
+  selectSubArrayAA2Core();
+  selectObservationTypeZoom();
+  enterSpectralAverageValue(0);
+  verifySpectralAverageRangeError();
+  enterSpectralAverageValue(865);
+  verifySpectralAverageRangeError();
+};
+
+const verifyZoomSpectralAverageRangeCustom = value => {
+  selectSubArrayCustom();
+  enterSpectralAverageValue(0);
+  verifySpectralAverageRangeError();
+  enterSpectralAverageValue(865);
+  verifySpectralAverageRangeError();
+};
 const enterSpectralAverageValue = value => {
   cy.get('[data-testid="spectralAveraging"]').type('{selectall}{del}');
   cy.get('[data-testid="spectralAveraging"]').type(value);
@@ -65,9 +85,24 @@ const selectSubArrayAA2 = value => {
   cy.get('[data-value="3"]').click({ force: true });
 };
 
+const selectSubArrayAA2Core = value => {
+  cy.get('[data-testid="subArrayConfiguration"]').click();
+  cy.get('[data-value="4"]').click({ force: true });
+};
+
 const selectSubArrayAA1 = value => {
   cy.get('[data-testid="subArrayConfiguration"]').click();
   cy.get('[data-value="2"]').click({ force: true });
+};
+
+const selectSubArrayCustom = value => {
+  cy.get('[data-testid="subArrayConfiguration"]').click();
+  cy.get('[data-value="20"]').click({ force: true });
+};
+
+const selectObservationTypeZoom = value => {
+  cy.get('[data-testid="observationType"]').click();
+  cy.get('[data-value="0"]').click({ force: true });
 };
 
 const verifySpectralAverageRangeError = () => {
