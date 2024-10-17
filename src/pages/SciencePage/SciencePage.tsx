8051a485137bd11cfe26b568534d7af11a3b2435
Chloe <chloe.gallacher@cgi.com>
2024-10-17 16:45:25 +0100

    STAR-748: Refactor Drag drop upload, filename functionality

--- src/pages/SciencePage/SciencePage.tsx
+++ src/pages/SciencePage/SciencePage.tsx
@@ -66,7 +66,7 @@ export default function SciencePage() {
       setCurrentFile(theFile);
       console.log('Chloe 1 ', theFile);
       console.log('Chloe 2 ', theFile.name);
-      setName(theFile.name);
+      setName(theFile.toString);
     } else {
       setProposal((({ sciencePDF, ...rest }) => rest)(getProposal()));
       setCurrentFile(null);
@@ -162,10 +162,9 @@ export default function SciencePage() {
   const NotifyError = (str: string) => Notify(str, AlertColorTypes.Error);
   const NotifyOK = (str: string) => Notify(str, AlertColorTypes.Success);
 
-  const displayName = () => (name?.length > 20 ? name.substring(0, 20) + '...' : name);
   const showFileName = () => (
     <Typography pt={1} data-testid={'fileUploadFilename'} variant="body1">
-      CHLOE FILENAME ={name?.length ? displayName() : ''}
+      CHLOE FILENAME ={name}
     </Typography>
   );
 
