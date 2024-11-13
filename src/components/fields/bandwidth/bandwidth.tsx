f9caadc883fd056285c61ab0e2ca0a0e0522d66f
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-11-13 17:19:18 +0000

    STAR-844 - extract bandwidth units

--- src/components/fields/bandwidth/bandwidth.tsx
+++ src/components/fields/bandwidth/bandwidth.tsx
@@ -61,55 +61,61 @@ export default function BandwidthField({
       };
     });
 
-    const scaleBandwidthOrFrequency = (incValue: number, incUnits: number): number => {
-      const frequencyUnitsLabel = FREQUENCY_UNITS.find(item => item.value === incUnits)?.label;
-      return sensCalHelpers.format.convertBandwidthToHz(incValue, frequencyUnitsLabel);
-    };
+  const scaleBandwidthOrFrequency = (incValue: number, incUnits: number): number => {
+    const frequencyUnitsLabel = FREQUENCY_UNITS.find(item => item.value === incUnits)?.label;
+    return sensCalHelpers.format.convertBandwidthToHz(incValue, frequencyUnitsLabel);
+  };
 
-    const errorMessage = () => {
-      const   bandwidthUnits = 0; // TODO extract bandwidth units from field
-      console.log('bandwidth', value);
+  const getBandwidthUnits = () => {
+    // OBSERVATION.array[telescope - 1].bandWidth
+    return OBSERVATION.array[telescope - 1].bandWidth.find(bw => bw.value === value)?.mapping;
+  }
 
-      // scale bandwidth and frequency
-      //const scaledBandwidth = scaleBandwidthOrFrequency(value, bandwidthUnits);
-      //const scaledFrequency = scaleBandwidthOrFrequency(centralFrequency, centralFrequencyUnits);
-  
-      // The bandwidth should be greater than the fundamental limit of the bandwidth provided by SKA MID or LOW
-      /* const minimumChannelWidthHz = getMinimumChannelWidth();
+  const errorMessage = () => {
+    const bandwidthUnits = getBandwidthUnits(); // TODO extract bandwidth units from field
+    console.log('bandwidthUnits', bandwidthUnits);
+    console.log('bandwidth', value);
+
+    // scale bandwidth and frequency
+    //const scaledBandwidth = scaleBandwidthOrFrequency(value, bandwidthUnits);
+    //const scaledFrequency = scaleBandwidthOrFrequency(centralFrequency, centralFrequencyUnits);
+
+    // The bandwidth should be greater than the fundamental limit of the bandwidth provided by SKA MID or LOW
+    /* const minimumChannelWidthHz = getMinimumChannelWidth();
       if (scaledBandwidth < minimumChannelWidthHz) {
         return displayMinimumChannelWidthErrorMessage(minimumChannelWidthHz);
       } */
-  
-      // The bandwidth should be smaller than the maximum bandwidth defined for the subarray
-      // For the subarrays that don't have one set, the full bandwidth is allowed
-      /* const maxContBandwidthHz = getMaxContBandwidthHz();
+
+    // The bandwidth should be smaller than the maximum bandwidth defined for the subarray
+    // For the subarrays that don't have one set, the full bandwidth is allowed
+    /* const maxContBandwidthHz = getMaxContBandwidthHz();
       if (maxContBandwidthHz && scaledBandwidth > maxContBandwidthHz) {
         return displayMaxContBandwidthErrorMessage(maxContBandwidthHz);
       } */
-  
-      // The bandwidth's lower and upper bounds should be within band limits
-      // Lower and upper bounds are set as frequency -/+ half bandwidth
-      // The band limits for each antennas (ska/meerkat/mixed) are set for each band (Mid)
-      // The antennas depend on the subarray selected
-      /* const halfBandwidth = scaledBandwidth / 2.0;
+
+    // The bandwidth's lower and upper bounds should be within band limits
+    // Lower and upper bounds are set as frequency -/+ half bandwidth
+    // The band limits for each antennas (ska/meerkat/mixed) are set for each band (Mid)
+    // The antennas depend on the subarray selected
+    /* const halfBandwidth = scaledBandwidth / 2.0;
       const lowerBound: number = scaledFrequency - halfBandwidth;
       const upperBound: number = scaledFrequency + halfBandwidth;
       const bandLimits = getBandLimits();
       if ((bandLimits && lowerBound < bandLimits[0]) || (bandLimits && upperBound > bandLimits[1])) {
         return t('continuumBandWidth.range.rangeError');
       } */
-  
-      // The sub-band bandwidth defined by the bandwidth of the observation divided by the number of
-      // sub-bands should be greater than the minimum allowed bandwidth
-      // Mid only
-      /* if (!isLow() && nSubBands && scaledBandwidth / nSubBands < minimumChannelWidthHz) {
+
+    // The sub-band bandwidth defined by the bandwidth of the observation divided by the number of
+    // sub-bands should be greater than the minimum allowed bandwidth
+    // Mid only
+    /* if (!isLow() && nSubBands && scaledBandwidth / nSubBands < minimumChannelWidthHz) {
         return t('continuumBandWidth.range.subBandError');
       } */
-  
-      return 'wrong';
-      // TODO : handle band5a NaN cases?
-      // TODO : handle zooms
-    };
+
+    return 'wrong';
+    // TODO : handle band5a NaN cases?
+    // TODO : handle zooms
+  };
 
   return (
     <Grid pt={1} spacing={0} container justifyContent="space-between" direction="row">
