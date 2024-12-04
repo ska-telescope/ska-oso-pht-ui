350e0c61782a714681ac870ca9b22b6379371042
Jack Tam <jack.tam@akkodis.com>
2024-12-04 09:19:27 +0000

    STAR-881: bump patch release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, Stargazer'
 
 # The short X.Y version
-version = '1.0.3'
+version = '1.0.4'
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = '1.0.4'
 
 # -- General configuration ---------------------------------------------------
 
