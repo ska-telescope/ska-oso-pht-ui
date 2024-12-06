e2c46b9dcb35cdb29656a538e5ca225ab529a8dc
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2024-12-06 13:31:10 +0000

    STAR-810 - remove console.log

--- src/pages/ObservationPage/ObservationPage.tsx
+++ src/pages/ObservationPage/ObservationPage.tsx
@@ -171,7 +171,6 @@ export default function ObservationPage() {
   const getSensCalcData = async (observation: Observation, target: Target) => {
     const response = await getSensCalc(observation, target);
     if (response) {
-      console.log('getSensCalcData', response);
       setSensCalc(response, target, observation.id);
     }
   };
