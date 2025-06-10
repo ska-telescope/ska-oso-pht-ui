d8a781fba730fd17b6da07a18ddd1a8d7483413c
Chloe <chloe.gallacher@cgi.com>
2025-06-10 15:24:49 +0100

    STAR-1433: Add component tests for helper functions

--- src/utils/types/helpers.test.tsx
+++ src/utils/types/helpers.test.tsx
@@ -1,6 +1,11 @@
 import { describe, test } from 'vitest';
 import '@testing-library/jest-dom';
-import { countWords, generateId } from '@/utils/helpers.ts';
+import {
+  calculateVelocity,
+  countWords,
+  generateId,
+  getMinimumChannelWidth
+} from '@/utils/helpers.ts';
 
 describe('PHT UI helper functions', () => {
   test('Generate ID', () => {
@@ -10,5 +15,17 @@ describe('PHT UI helper functions', () => {
 
   test('Count words', () => {
     expect(countWords('An example sentence.')).toBe(3);
+  });
+
+  test('Minimum channel width, telescope MID, value 1', () => {
+    expect(getMinimumChannelWidth(1)).toBe(13440);
+  });
+
+  test('Minimum channel width, telescope LOW, value 2', () => {
+    expect(getMinimumChannelWidth(2)).toBe(5425.347222222223);
+  });
+
+  test('Velocity', () => {
+    expect(calculateVelocity(200, 300, 2)).toBe('199861.64 km/s');
   });
 });
