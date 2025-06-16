9e59957cb7de4fc87d24cad3359b9320bead1325
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-06-16 17:23:22 +0100

    STAR-1219 - fix tests

--- src/pages/PHT/PHT.test.tsx
+++ src/pages/PHT/PHT.test.tsx
@@ -2,14 +2,17 @@ import { describe, test } from 'vitest';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
+import { MemoryRouter } from 'react-router-dom';
 import PHT from './PHT';
 
 describe('<PHT />', () => {
   test('renders correctly', () => {
     render(
-      <StoreProvider>
-        <PHT />
-      </StoreProvider>
+      <MemoryRouter>
+        <StoreProvider>
+          <PHT />
+        </StoreProvider>
+      </MemoryRouter>
     );
   });
 });
