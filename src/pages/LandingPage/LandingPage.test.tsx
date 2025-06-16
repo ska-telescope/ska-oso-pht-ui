d703786dc713ac0139ac929fd869d91dc84be844
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-06-16 13:59:17 +0100

    STAR-1219 - reinstate landing page and put maintenance page into panenMaintenance page

--- src/pages/LandingPage/LandingPage.test.tsx
+++ src/pages/LandingPage/LandingPage.test.tsx
@@ -2,7 +2,7 @@ import { describe, test } from 'vitest';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
-import LandingPage from './LandingPage';
+import LandingPage from '../PanelMaintenance/LandingPage';
 
 describe('<LandingPage />', () => {
   test('renders correctly', () => {
