743dd6caa8fcaa2b9e816f75247bd429511783a2
Trevor Swain <trevor.swain@cgi.com>
2025-06-26 17:35:49 +0100

    STAR-1383 : Sorted out failing test

--- src/pages/ReviewPage/ReviewPage.test.tsx
+++ src/pages/ReviewPage/ReviewPage.test.tsx
@@ -2,13 +2,13 @@ import { describe, test } from 'vitest';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
-import PanelReviewDecision from './PanelReviewDecision';
+import ReviewPage from './ReviewPage';
 
-describe('<PanelReviewDecision />', () => {
+describe('<ReviewPage />', () => {
   test('renders correctly', () => {
     render(
       <StoreProvider>
-        <PanelReviewDecision />
+        <ReviewPage />
       </StoreProvider>
     );
   });
