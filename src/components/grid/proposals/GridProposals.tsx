78b5657cc1ac6868264399ff19eafd50243fefc3
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-07-03 10:26:30 +0100

    STAR-1367 - lint

--- src/components/grid/proposals/GridProposals.tsx
+++ src/components/grid/proposals/GridProposals.tsx
@@ -545,7 +545,11 @@ export default function GridProposals({
       )}
       <Grid2 size={{ xs: 12 }} pt={1}>
         {!axiosViewError && (!filteredData || filteredData?.length === 0) && (
-          <Alert color={AlertColorTypes.Info} text={t('proposals.empty')} testId="helpProposalsId" />
+          <Alert
+            color={AlertColorTypes.Info}
+            text={t('proposals.empty')}
+            testId="helpProposalsId"
+          />
         )}
         {!axiosViewError && filteredData.length > 0 && (
           <div>
