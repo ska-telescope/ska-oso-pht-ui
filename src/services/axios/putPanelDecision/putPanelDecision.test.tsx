d73767319aa80f2faf5f091a0b8b1c96dc4dfd6f
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-07-14 17:22:17 +0100

    STAR-1503 - unit tests

--- src/services/axios/putPanelDecision/putPanelDecision.test.tsx
+++ src/services/axios/putPanelDecision/putPanelDecision.test.tsx
@@ -48,33 +48,31 @@ describe('PostPanelDecision Service', () => {
     expect(result).to.deep.equal(MockPanelDecisionBackend);
   });
 
-  /*
   test('returns error message on API failure', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAxios.post.mockRejectedValue(new Error('Network Error'));
-    const result = await PostPanelDecision(MockPanelDecisionFrontend);
+    mockedAxios.put.mockRejectedValue(new Error('Network Error'));
+    const result = await PutPanelDecision(MockPanelDecisionFrontend.id, MockPanelDecisionFrontend);
     expect(result).toStrictEqual({ error: 'Network Error' });
   });
 
   test('returns error.API_UNKNOWN_ERROR when thrown error is not an instance of Error', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAxios.post.mockRejectedValue({ unexpected: 'object' });
-    const result = await PostPanelDecision(MockPanelDecisionFrontend);
+    mockedAxios.put.mockRejectedValue({ unexpected: 'object' });
+    const result = await PutPanelDecision(MockPanelDecisionFrontend.id, MockPanelDecisionFrontend);
     expect(result).toStrictEqual({ error: 'error.API_UNKNOWN_ERROR' });
   });
 
   test('returns error.API_UNKNOWN_ERROR when result undefined', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAxios.post.mockResolvedValue(undefined);
-    const result = await PostPanelDecision(MockPanelDecisionFrontend);
+    mockedAxios.put.mockResolvedValue(undefined);
+    const result = await PutPanelDecision(MockPanelDecisionFrontend.id, MockPanelDecisionFrontend);
     expect(result).toStrictEqual({ error: 'error.API_UNKNOWN_ERROR' });
   });
 
   test('returns error.API_UNKNOWN_ERROR when result null', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAxios.post.mockResolvedValue(null);
-    const result = await PostPanelDecision(MockPanelDecisionFrontend);
+    mockedAxios.put.mockResolvedValue(null);
+    const result = await PutPanelDecision(MockPanelDecisionFrontend.id, MockPanelDecisionFrontend);
     expect(result).toStrictEqual({ error: 'error.API_UNKNOWN_ERROR' });
   });
-  */
 });
