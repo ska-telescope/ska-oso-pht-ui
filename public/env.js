11319bde607fbecfe9c5f8386755d272f5f0742e
Trevor Swain <trevor.swain@cgi.com>
2025-07-21 10:41:17 +0100

    STAR-1363 : Added missing data to Review list

--- public/env.js
+++ public/env.js
@@ -2,7 +2,6 @@ window.env = {
   "REACT_APP_SKA_PHT_BASE_URL": "",
   "REACT_APP_SKA_OSO_SERVICES_URL":'http://192.168.49.2/ska-oso-services/oso/api/v1',
   "REACT_APP_SKA_SENSITIVITY_CALC_URL": "http://192.168.49.2/ska-ost-senscalc/api/v11/",
-  "REACT_APP_SKA_SENSITIVITY_CALC_URL_SAVE": "http://192.168.49.2/ska-ost-senscalc/api/v11/",
   "REACT_APP_USE_LOCAL_DATA_SENSITIVITY_CALC": "false",
   "REACT_APP_USE_LOCAL_DATA": "false",
   "REACT_APP_DOMAIN": "https://sdhp.stfc.skao.int",
--- src/pages/PanelReviewDecisionList/PanelReviewDecisionList.tsx
+++ src/pages/PanelReviewDecisionList/PanelReviewDecisionList.tsx
@@ -1,38 +1,111 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Grid2, Paper } from '@mui/material';
-import { DropDown, SearchEntry } from '@ska-telescope/ska-gui-components';
+import { AlertColorTypes, DropDown, SearchEntry } from '@ska-telescope/ska-gui-components';
 import { Spacer, SPACER_VERTICAL } from '@ska-telescope/ska-gui-components';
+import { storageObject } from '@ska-telescope/ska-gui-local-storage';
 import GetProposalList from '../../services/axios/getProposalList/getProposalList';
 import { SEARCH_TYPE_OPTIONS, BANNER_PMT_SPACER } from '../../utils/constants';
 import Proposal from '../../utils/types/proposal';
 import { FOOTER_SPACER } from '../../utils/constants';
 
+import Notification from '../../utils/types/notification';
 import PageBannerPMT from '@/components/layout/pageBannerPMT/PageBannerPMT';
 import SubmitButton from '@/components/button/Submit/Submit';
 import TableReviewDecision from '@/components/grid/tableReviewDecision/TableReviewDecision';
+import { ProposalReview } from '@/utils/types/proposalReview';
+import GetPanelList from '@/services/axios/getPanelList/getPanelList';
+import GetProposalReviewList from '@/services/axios/getProposalReviewList/getProposalReviewList';
+import { Panel } from '@/utils/types/panel';
+
+/*
+ * Process for retrieving the data for the list
+ *
+ * 1. Fetch the list of proposals IDs that are in the panel that the user is in
+ * 2. For each proposal ID, fetch the details of the proposal
+ * 3. Fetch the details of the proposal's review decisions
+ * 4. Combine the data into a single array of objects
+ *
+ * NOTE
+ * Step 1 : There is not a endpoint to retrieve all proposals by status, so all are currently retrieved
+ *
+ * Step 2 is currently inefficient as the appropriate endpoint is not available
+ * In the meantime, the list of proposals is being retrieved and being filtered
+ */
 
 export default function ReviewDecisionListPage() {
   const { t } = useTranslation('pht');
+  const { updateAppContent5 } = storageObject.useStore();
 
   const [searchTerm, setSearchTerm] = React.useState('');
   const [searchType, setSearchType] = React.useState('');
+
+  const [reset, setReset] = React.useState(false);
+  const [panelData, setPanelData] = React.useState<Panel[]>([]);
   const [proposals, setProposals] = React.useState<Proposal[]>([]);
-  const [fetchList, setFetchList] = React.useState(false);
+  const [proposalReviews, setProposalReviews] = React.useState<ProposalReview[]>([]);
+
+  /*--------------------------------------------------------------------------*/
+
+  function Notify(str: string, lvl = AlertColorTypes.Info) {
+    const rec: Notification = {
+      level: lvl,
+      message: str
+    };
+    updateAppContent5(rec);
+  }
+
+  const NotifyError = (str: string) => Notify(str, AlertColorTypes.Error);
+  // const NotifyOK = (str: string) => Notify(str, AlertColorTypes.Success);
+
+  /*--------------------------------------------------------------------------*/
 
   React.useEffect(() => {
-    setFetchList(!fetchList);
+    setReset(!reset);
   }, []);
 
   React.useEffect(() => {
-    const fetchData = async () => {
-      const response = await GetProposalList();
-      if (typeof response !== 'string') {
-        setProposals(response);
+    const GetReviewPanels = async () => {
+      const response = await GetPanelList(); // TODO : Add the user_id as a property to the function
+      if (typeof response === 'string') {
+        NotifyError(response);
+      } else {
+        setPanelData((response as unknown) as Panel[]);
       }
     };
-    fetchData();
-  }, [fetchList]);
+    GetReviewPanels();
+  }, [reset]);
+
+  React.useEffect(() => {
+    const fetchProposalData = async () => {
+      const response = await GetProposalList(); // TODO : Temporary implementation to get all proposals
+      if (typeof response === 'string') {
+        NotifyError(response);
+      } else {
+        const panelProposalIds = panelData.flatMap(panel =>
+          Array.isArray(panel.proposals) ? panel.proposals.map(proposal => proposal.proposalId) : []
+        );
+        const filtered = response
+          ? response.filter((proposal: Proposal) => panelProposalIds.includes(proposal.id))
+          : [];
+        setProposals(filtered);
+      }
+    };
+    const fetchProposalReviewData = async () => {
+      const response = await GetProposalReviewList(); // TODO : add id of the logged in user
+      if (typeof response === 'string') {
+        NotifyError(response);
+      } else {
+        setProposalReviews(response);
+      }
+    };
+    fetchProposalData();
+    fetchProposalReviewData();
+  }, [panelData]);
+
+  // const getUser = () => 'DefaultUser'; // TODO
+
+  /*--------------------------------------------------------------------------*/
 
   function filterProposals() {
     return proposals.filter(item => {
@@ -48,6 +121,7 @@ export default function ReviewDecisionListPage() {
   }
 
   const filteredData = proposals ? filterProposals() : [];
+  // console.log('Filtered Data:', filteredData);
 
   const searchDropdown = () => (
     <DropDown
