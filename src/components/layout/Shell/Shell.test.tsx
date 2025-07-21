b38f5dd1268a8d8a274edb5ff72e00348cb8060b
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-07-21 12:38:52 +0100

    STAR-1526 - fix tests

--- src/components/layout/Shell/Shell.test.tsx
+++ src/components/layout/Shell/Shell.test.tsx
@@ -3,12 +3,15 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 import Shell from './Shell';
+import { MockedLoginProvider } from '@/contexts/MockedLoginContext';
 
 describe('<Shell />', () => {
   test('renders correctly', () => {
     render(
       <StoreProvider>
-        <Shell page={0} />
+        <MockedLoginProvider>
+          <Shell page={0} />
+        </MockedLoginProvider>
       </StoreProvider>
     );
   });
