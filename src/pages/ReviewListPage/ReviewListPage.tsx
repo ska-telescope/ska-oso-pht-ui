99713b1b51474a793779ea37d9c4eba5a4494b44
Chloe <chloe.gallacher@cgi.com>
2025-08-01 11:27:37 +0100

    STAR-1511: Disable science review if technical feasibility = No

--- src/pages/ReviewListPage/ReviewListPage.tsx
+++ src/pages/ReviewListPage/ReviewListPage.tsx
@@ -15,7 +15,7 @@ import {
   SEARCH_TYPE_OPTIONS,
   BANNER_PMT_SPACER,
   PANEL_DECISION_STATUS,
-  REVIEW_TYPE
+  REVIEW_TYPE, FEASIBILITY, TECHNICAL_FEASIBILITY_OPTIONS
 } from '@utils/constants.ts';
 import { validateProposal } from '@utils/proposalValidation.tsx';
 import GetProposalList from '../../services/axios/getProposalList/getProposalList';
@@ -229,8 +229,10 @@ export default function ReviewListPage() {
     updateReview(row);
   };
 
-  const canEditScience = (e: { row: { status: string } }) =>
-    e.row.status !== PANEL_DECISION_STATUS.DECIDED;
+  const canEditScience = (e: { row: { status: string, feasibility: string } }) => {
+    return e?.row?.feasibility !== FEASIBILITY[1] && e.row.status !== PANEL_DECISION_STATUS.DECIDED
+  }
+
   const canEditTechnical = (e: { row: { status: string } }) =>
     e.row.status !== PANEL_DECISION_STATUS.DECIDED;
   const canSubmit = (row: { srcNet: any; comments: any; rank: number; status: string }) =>
