c1a9ead0cf4b8c870070ae4bcabd441800865aa8
Chloe <chloe.gallacher@cgi.com>
2025-08-01 12:37:36 +0100

    STAR-1511: Update test

--- src/pages/entry/ReviewEntry/ReviewEntry.test.tsx
+++ src/pages/entry/ReviewEntry/ReviewEntry.test.tsx
@@ -3,12 +3,21 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 import ReviewEntry from './ReviewEntry';
+import { REVIEW_TYPE } from '@utils/constants.ts';
 
 describe('<ReviewEntry />', () => {
-  test('renders correctly', () => {
+  test('renders correctly, Review type Science', () => {
     render(
       <StoreProvider>
-        <ReviewEntry />
+        <ReviewEntry  reviewType={REVIEW_TYPE.SCIENCE}/>
+      </StoreProvider>
+    );
+  });
+
+  test('renders correctly, Review type Technical', () => {
+    render(
+      <StoreProvider>
+        <ReviewEntry  reviewType={REVIEW_TYPE.TECHNICAL}/>
       </StoreProvider>
     );
   });
