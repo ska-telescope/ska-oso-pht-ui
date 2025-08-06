b2212cda364ca0d23cc9668fe4bc173f87f2da9b
Chloe <chloe.gallacher@cgi.com>
2025-08-06 14:53:51 +0100

    STAR-1511: Tidy up lint

--- src/pages/entry/ReviewEntry/ReviewEntry.test.tsx
+++ src/pages/entry/ReviewEntry/ReviewEntry.test.tsx
@@ -1,10 +1,8 @@
-import { describe, expect, test } from 'vitest';
+import { describe, test } from 'vitest';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 import { REVIEW_TYPE } from '@utils/constants.ts';
-import { addProposalPanel } from '@pages/PanelMaintenance/PanelMaintenance.tsx';
-import MockProposalFrontendList from '@services/axios/getProposalList/mockProposalFrontendList.tsx';
 import ReviewEntry from './ReviewEntry';
 
 describe('<ReviewEntry />', () => {
