0c1e2d1e5baa9ca28af35d07261068184f58b06c
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-08-14 22:05:06 +0100

    STAR-1609 - fix tests

--- src/pages/TeamPage/MemberSearch/MemberSearch.test.tsx
+++ src/pages/TeamPage/MemberSearch/MemberSearch.test.tsx
@@ -1,8 +1,8 @@
 import { describe, test } from 'vitest';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import MemberSearch from './MemberSearch';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
+import MemberSearch from './MemberSearch';
 
 describe('<MemberSearch />', () => {
   test('renders correctly', () => {
