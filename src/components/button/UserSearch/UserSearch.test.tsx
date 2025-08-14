efb619591d0cfc2c75a72295f7ec95a77d5afac0
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-08-14 15:16:53 +0100

    STAR-1609 - add userSearch button

---
+++ src/components/button/UserSearch/UserSearch.test.tsx
@@ -0,0 +1,22 @@
+import { describe, expect, test, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import UserSearchButton from './UserSearch';
+import '@testing-library/jest-dom';
+
+describe('<UserSearchButton />', () => {
+  const mockAction = vi.fn();
+  test('renders correctly', () => {
+    render(<UserSearchButton action={mockAction} />);
+    expect(screen.getByTestId('investigatorSearchButtonTestId')).toHaveTextContent(
+      'sendInviteBtn.label'
+    );
+    screen.getByTestId('investigatorSearchButtonTestId').click();
+    expect(mockAction).toBeCalled();
+  });
+  test('renders correctly with tooltip empty', () => {
+    render(<UserSearchButton action={mockAction} toolTip="" />);
+    expect(screen.getByTestId('investigatorSearchButtonTestId')).toHaveTextContent(
+      'sendInviteBtn.label'
+    );
+  });
+});
