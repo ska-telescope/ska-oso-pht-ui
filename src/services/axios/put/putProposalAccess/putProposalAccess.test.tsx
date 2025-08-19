b7061e3bcb260de00a4c69674e105662ba958a95
Chloe <chloe.gallacher@cgi.com>
2025-08-19 16:40:11 +0100

    STAR-1559: Add unit tests for put proposal access and update tests for post proposal access

--- src/services/axios/put/putProposalAccess/putProposalAccess.test.tsx
+++ src/services/axios/put/putProposalAccess/putProposalAccess.test.tsx
@@ -1,21 +1,23 @@
 import { describe, test, expect } from 'vitest';
 import '@testing-library/jest-dom';
-import PostProposalAccess from '@services/axios/post/postProposalAccess/postProposalAccess.tsx';
+import PutProposalAccess from '@services/axios/put/putProposalAccess/putProposalAccess.tsx';
+import MockProposalAccessFrontend from '@services/axios/put/putProposalAccess/mockProposalAccessFrontend.tsx';
+import MockProposalAccessBackend from '@services/axios/put/putProposalAccess/mockProposalAccessBackend.tsx';
 import * as CONSTANTS from '@/utils/constants';
 
-describe('PostProposalAccess Service', () => {
+describe('PutProposalAccess Service', () => {
   let mockedAuthClient: any;
   beforeEach(() => {
     vi.resetAllMocks();
     mockedAuthClient = {
-      post: vi.fn()
+      put: vi.fn()
     };
   });
 
   test('returns mocked data when USE_LOCAL_DATA is true', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(true);
-    const result = await PostProposalAccess(mockedAuthClient, 'dummy');
-    expect(result).toEqual('PROPOSAL-ACCESS-ID-001');
+    const result = await PutProposalAccess(mockedAuthClient, MockProposalAccessBackend);
+    expect(result).toEqual(MockProposalAccessFrontend);
   });
 
   test('returns data from API when USE_LOCAL_DATA is false', async () => {
@@ -24,29 +26,29 @@ describe('PostProposalAccess Service', () => {
 
   test('returns error message on API failure', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAuthClient.post.mockRejectedValue(new Error('Network Error'));
-    const result = await PostProposalAccess(mockedAuthClient, 'dummy');
+    mockedAuthClient.put.mockRejectedValue(new Error('Network Error'));
+    const result = await PutProposalAccess(mockedAuthClient, MockProposalAccessBackend);
     expect(result).toStrictEqual({ error: 'Network Error' });
   });
 
   test('returns error.API_UNKNOWN_ERROR when thrown error is not an instance of Error', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAuthClient.post.mockRejectedValue({ unexpected: 'object' });
-    const result = await PostProposalAccess(mockedAuthClient, 'dummy');
+    mockedAuthClient.put.mockRejectedValue({ unexpected: 'object' });
+    const result = await PutProposalAccess(mockedAuthClient, MockProposalAccessBackend);
     expect(result).toStrictEqual({ error: 'error.API_UNKNOWN_ERROR' });
   });
 
   test('returns error.API_UNKNOWN_ERROR when result undefined', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAuthClient.post.mockResolvedValue(undefined);
-    const result = await PostProposalAccess(mockedAuthClient, 'dummy');
+    mockedAuthClient.put.mockResolvedValue(undefined);
+    const result = await PutProposalAccess(mockedAuthClient, MockProposalAccessBackend);
     expect(result).toStrictEqual({ error: 'error.API_UNKNOWN_ERROR' });
   });
 
   test('returns error.API_UNKNOWN_ERROR when result null', async () => {
     vi.spyOn(CONSTANTS, 'USE_LOCAL_DATA', 'get').mockReturnValue(false);
-    mockedAuthClient.post.mockResolvedValue(null);
-    const result = await PostProposalAccess(mockedAuthClient, 'dummy');
+    mockedAuthClient.put.mockResolvedValue(null);
+    const result = await PutProposalAccess(mockedAuthClient, MockProposalAccessBackend);
     expect(result).toStrictEqual({ error: 'error.API_UNKNOWN_ERROR' });
   });
 });
