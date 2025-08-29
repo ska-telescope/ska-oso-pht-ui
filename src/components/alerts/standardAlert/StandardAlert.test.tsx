f1ca633603f279367d1421907aa27cafd02d2198
Chloe <chloe.gallacher@cgi.com>
2025-08-29 16:09:10 +0100

    STAR-1559: Resolve lint

--- src/components/alerts/standardAlert/StandardAlert.test.tsx
+++ src/components/alerts/standardAlert/StandardAlert.test.tsx
@@ -1,5 +1,5 @@
 import { describe, test } from 'vitest';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 import '@testing-library/jest-dom';
 import { AlertColorTypes } from '@ska-telescope/ska-gui-components';
