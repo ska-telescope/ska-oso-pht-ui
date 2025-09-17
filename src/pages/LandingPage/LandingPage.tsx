1add5d6f74ea45a355153ab4e7f80f4fcbff0e0d
Chloe <chloe.gallacher@cgi.com>
2025-09-17 16:24:11 +0100

    STAR-1508: Refactor landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -478,10 +478,10 @@ export default function LandingPage() {
           {loggedIn || cypressToken ? addProposalButton() : addMockButton()}
         </Grid>
         <Grid size={{ sm: 4 }} p={2}>
-          {loggedIn || (cypressToken && searchDropdown())}
+          {!loggedIn || (!cypressToken && searchDropdown())}
         </Grid>
         <Grid size={{ sm: 4, md: 6, lg: 6 }} p={2} mt={-1}>
-          {loggedIn || (cypressToken && searchEntryField('searchId'))}
+          {!loggedIn || (!cypressToken && searchEntryField('searchId'))}
         </Grid>
         <Grid size={{ xs: 12 }} pt={1}>
           {!axiosViewError && (!filteredData || filteredData.length === 0) && (
