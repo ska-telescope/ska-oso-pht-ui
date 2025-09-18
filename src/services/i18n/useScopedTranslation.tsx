dba021baa5cf03773e1c7010360c7755d7a09e30
Trevor Swain <trevor.swain@cgi.com>
2025-09-18 12:26:08 +0100

    STAR-1661 : Annotated out the code that does the testing

--- src/services/i18n/useScopedTranslation.tsx
+++ src/services/i18n/useScopedTranslation.tsx
@@ -1,16 +1,16 @@
-import { storageObject } from '@ska-telescope/ska-gui-local-storage';
+// TODO : Put these back for implementation import { storageObject } from '@ska-telescope/ska-gui-local-storage';
 import { useTranslation } from 'react-i18next';
-import ObservatoryData from '@/utils/types/observatoryData';
-import { isCypress } from '@/utils/constants';
+// TODO : Put these back for implementation import ObservatoryData from '@/utils/types/observatoryData';
+// TODO : Put these back for implementation import { isCypress } from '@/utils/constants';
 
 const isDev = process.env.NODE_ENV === 'development';
 
 export const useScopedTranslation = (namespaces?: string[]) => {
-  const { application } = storageObject.useStore();
-  const getObservatoryData = () => application.content3 as ObservatoryData;
-  const isSV = () =>
-    !isCypress &&
-    getObservatoryData()?.observatoryPolicy?.cycleDescription === 'Science Verification';
+  // TODO : Put these back for implementation const { application } = storageObject.useStore();
+  // TODO : Put these back for implementation const getObservatoryData = () => application.content3 as ObservatoryData;
+  const isSV = () => false;
+  // TODO : Put these back for implementation !isCypress &&
+  // TODO : Put these back for implementation getObservatoryData()?.observatoryPolicy?.cycleDescription === 'Science Verification';
 
   const defaultNamespaces = isSV() ? ['sv', 'pht'] : ['pht'];
   const { t: rawT, ...rest } = useTranslation(namespaces || defaultNamespaces);
