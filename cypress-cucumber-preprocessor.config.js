774ab33b1fe31974c153f879e1be071aac119759
Chloe <chloe.gallacher@cgi.com>
2025-09-18 14:24:01 +0100

    STAR-1581: WIP, resolve xray

---
+++ cypress-cucumber-preprocessor.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  json: {
+    enabled: true,
+    output: 'cucumber-report.json',
+  },
+};
--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,38 +1,37 @@
-import { defineConfig } from 'cypress';
-import vitePreprocessor from 'cypress-vite';
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
-export default defineConfig({
-  video: false,
-  projectId: 'ssiwb9', //projectId to enable cypress cloud
-  fixturesFolder: 'tests/cypress/fixtures',
-  screenshotsFolder: 'tests/cypress/artefacts/screenshots',
-  videosFolder: 'tests/cypress/artefacts/videos',
-  downloadsFolder: 'tests/cypress/artefacts/downloads',
-  reporter: "junit-xray-cypress-reporter",
-  reporterOptions: {
-    mochaFile: "tests/cypress/reports/test-output-[hash].xml"
-  },
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:6101',
-    //
-    defaultCommandTimeout: 10000,  // 4000
-    execTimeout: 120000, // 60000
-    taskTimeout: 120000, // 60000
-    pageLoadTimeout:  120000, // 60000
-    requestTimeout:  10000, // 5000
-    responseTimeout:  60000, // 30000
-    //
+    async setupNodeEvents(on, config) {
+      config.env = config.env || {}; // Ensure env is initialized
+      console.log('ENV:', config.env);
+
+      await addCucumberPreprocessorPlugin(config);
+
+      on('file:preprocessor', createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+
+      return config;
+    },
+    baseUrl: 'http://localhost:6100',
+    video: false,
+    fixturesFolder: 'tests/cypress/fixtures',
+    screenshotsFolder: 'tests/cypress/artefacts/screenshots',
+    videosFolder: 'tests/cypress/artefacts/videos',
+    downloadsFolder: 'tests/cypress/artefacts/downloads',
+    defaultCommandTimeout: 10000,
+    execTimeout: 120000,
+    taskTimeout: 120000,
+    pageLoadTimeout: 120000,
+    requestTimeout: 10000,
+    responseTimeout: 60000,
     experimentalRunAllSpecs: true,
     experimentalMemoryManagement: true,
     supportFile: 'tests/cypress/support/e2e.ts',
     specPattern: ['tests/cypress/e2e/**/*.test.{js,jsx,ts,tsx}'],
-    setupNodeEvents(on, config) {
-      on('file:preprocessor', vitePreprocessor());
-    }
   },
-
-  retries: {
-    runMode: 2,
-    openMode: 0
-  }
 });
