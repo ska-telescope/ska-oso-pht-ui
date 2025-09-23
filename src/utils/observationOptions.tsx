4d941cd86b1b0c3846220ecb5118a98fe1d2572a
Chloe <chloe.gallacher@cgi.com>
2025-09-23 08:45:36 +0100

    STAR-1562: Refactor observationOptions removing direct usage of constants

--- src/utils/observationOptions.tsx
+++ src/utils/observationOptions.tsx
@@ -1,8 +1,13 @@
-import { OBSERVATION } from '@utils/observationConstantData.ts';
+import { storageObject } from '@ska-telescope/ska-gui-local-storage';
+import ObservatoryData from '@utils/types/observatoryData.tsx';
+
+const { application} = storageObject.useStore();
+
+const getObservatoryData = () => application.content3 as ObservatoryData;
 
 export const subArrayOptions = (inObj: { telescope: number; isBand5: any }) => {
   if (!inObj) {
     return [];
   }
-  return OBSERVATION.array[inObj.telescope - 1]?.subarray;
+  return getObservatoryData().constantData?.array[inObj.telescope - 1]?.subarray;
 };
