d61062824cd45cb03f1ae9588d725bcdf6465f0f
Chloe <chloe.gallacher@cgi.com>
2025-09-23 11:54:32 +0100

    STAR-1562: Reset landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -16,7 +16,7 @@ import { presentDate, presentLatex, presentTime } from '@utils/present/present';
 import Investigator from '@utils/types/investigator.tsx';
 import PutProposal from '@services/axios/put/putProposal/putProposal';
 import GetProposal from '@services/axios/get/getProposal/getProposal';
-import ObservatoryData from '@utils/types/observatoryData.tsx';
+import ObservatoryData from '@/utils/types/observatoryData';
 import GetObservatoryData from '@/services/axios/get/getObservatoryData/getObservatoryData';
 import AddButton from '@/components/button/Add/Add';
 import CloneIcon from '@/components/icon/cloneIcon/cloneIcon';
