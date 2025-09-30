8954d291e297ae2263c34a7e8f1ace067162281e
Chloe <chloe.gallacher@cgi.com>
2025-09-30 15:17:14 +0100

    STAR-1665: Add table when multiple beams selected

--- src/components/fields/pulsarTimingBeam/PulsarTimingBeam.tsx
+++ src/components/fields/pulsarTimingBeam/PulsarTimingBeam.tsx
@@ -1,35 +1,55 @@
 import React from 'react';
 import { Radio, FormControlLabel } from '@mui/material';
+import { DataGrid } from '@ska-telescope/ska-gui-components';
+import { RA_TYPE_ICRS } from '@utils/constants.ts';
 import { useScopedTranslation } from '@/services/i18n/useScopedTranslation';
 
 export default function PulsarTimingBeamField() {
   const { t } = useScopedTranslation();
   const [selectedValue, setSelectedValue] = React.useState('noBeam');
+  const [showGrid, setShowGrid] = React.useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleClick = event => {
     setSelectedValue(event.target.value);
   };
 
-  const controlProps = (item: string) => ({
+  React.useEffect(() => {
+    setShowGrid(selectedValue === 'multipleBeams');
+  }, [selectedValue]);
+
+  const controlProps = item => ({
     checked: selectedValue === item,
-    onChange: handleChange,
+    onClick: handleClick,
     value: item,
-    name: 'color-radio-button-demo',
+    name: 'radio-group',
     inputProps: { 'aria-label': item }
   });
+
+  const columns = [
+    { field: 'name', headerName: t('name.label'), flex: 2 },
+    { field: 'raStr', headerName: t('skyDirection.short.1.' + RA_TYPE_ICRS.value), width: 120 },
+    { field: 'decStr', headerName: t('skyDirection.short.2.' + RA_TYPE_ICRS.value), width: 120 }
+  ];
+
+  const getColumns = () => columns;
 
   return (
     <div>
       <FormControlLabel
         control={<Radio {...controlProps('noBeam')} color="default" />}
         label={t('pulsarTimingBeam.noBeam.label')}
-        data-testid="No BeamTestId"
+        data-testid="NoBeamTestId"
       />
       <FormControlLabel
         control={<Radio {...controlProps('multipleBeams')} color="default" />}
         label={t('pulsarTimingBeam.multipleBeams.label')}
-        data-testid="Multiple BeamsTestId"
+        data-testid="MultipleBeamsTestId"
       />
+      {showGrid && (
+        <div style={{ height: 300, width: '100%' }}>
+          <DataGrid columns={getColumns()} height={171} testId="pulsarTimingBeamColumns" />
+        </div>
+      )}
     </div>
   );
 }
