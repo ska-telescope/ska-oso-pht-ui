d2aa1e6c47c6897159366c25051425798b0949c9
Chloe <chloe.gallacher@cgi.com>
2025-09-30 10:36:40 +0100

    STAR-1665: Update target labelling

--- src/components/info/extendedStrikethroughLabelBehindText/ExtendedStrikethroughLabelBehindText.tsx
+++ src/components/info/extendedStrikethroughLabelBehindText/ExtendedStrikethroughLabelBehindText.tsx
@@ -10,21 +10,22 @@ const ExtendedStrikethroughLabelBehindText: React.FC<ExtendedStrikethroughLabelB
     <Box
       sx={{
         position: 'relative',
-        display: 'inline-block',
-        px: 2,
+        width: '100%',
+        maxWidth: '400px',
+        height: '3rem',
+        mx: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-      <Typography component="span" sx={{ position: 'relative', zIndex: 2 }}>
-        {labelText}
-      </Typography>
-
-      {/* Strikethrough line */}
+      {/* Full-width line behind text */}
       <Box
         sx={{
           position: 'absolute',
           top: '50%',
-          left: '-10px',
-          right: '300px',
+          left: 0,
+          right: 0,
           height: '1px',
           backgroundColor: 'black',
           transform: 'translateY(-50%)',
@@ -32,35 +33,50 @@ const ExtendedStrikethroughLabelBehindText: React.FC<ExtendedStrikethroughLabelB
         }}
       />
 
-      {/* Left bullet */}
+      {/* Left bullet at start of line */}
       <Box
         sx={{
           position: 'absolute',
+          left: 0,
           top: '50%',
-          left: -10,
-          width: '15px',
-          height: '15px',
+          transform: 'translateY(-50%)',
+          width: '0.6rem',
+          height: '0.6rem',
           backgroundColor: 'black',
           borderRadius: '50%',
-          transform: 'translateY(-50%)',
-          zIndex: 1,
+          zIndex: 2,
         }}
       />
 
-      {/* Right bullet */}
+      {/* Right bullet at end of line */}
       <Box
         sx={{
           position: 'absolute',
+          right: 0,
           top: '50%',
-          right: '300px',
-          width: '15px',
-          height: '15px',
+          transform: 'translateY(-50%)',
+          width: '0.6rem',
+          height: '0.6rem',
           backgroundColor: 'black',
           borderRadius: '50%',
-          transform: 'translateY(-50%)',
-          zIndex: 1,
+          zIndex: 2,
         }}
       />
+
+      {/* Centered text */}
+      <Typography
+        component="span"
+        sx={{
+          px: 2,
+          zIndex: 2,
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          textAlign: 'center',
+        }}
+      >
+        {labelText}
+      </Typography>
     </Box>
   );
 };
