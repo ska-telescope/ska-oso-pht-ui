d91845a558db2bbb03b31af434b7adcb2988bb15
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-10-07 19:12:03 +0100

    STAR-1702 - bump

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, Stargazer'
 
 # The short X.Y version
-version = '6.0.0'
+version = '7.0.0'
 # The full version, including alpha/beta/rc tags
-release = '6.0.0'
+release = '7.0.0'
 
 # -- General configuration ---------------------------------------------------
 
