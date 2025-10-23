ef4fc21a510118b908a0f95d97dd255f278c673e
Trevor Swain <trevor.swain@cgi.com>
2025-10-23 17:36:11 +0100

    STAR-1751 : OSO-services update

---
+++ src/utils/appFlow/AppFlowContext.tsx
@@ -0,0 +1,35 @@
+// src/context/AppFlowContext.tsx
+import React, { createContext, useContext, useState } from 'react';
+
+export type AppFlowType = 'Science Verification' | 'Proposal';
+
+interface AppFlowContextType {
+  appFlow: AppFlowType;
+  isSV: () => boolean;
+  setAppFlow: (flow: AppFlowType) => void;
+}
+
+const AppFlowContext = createContext<AppFlowContextType | undefined>(undefined);
+
+export const AppFlowProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [appFlow, _setAppFlow] = useState<AppFlowType>('Science Verification');
+
+  const isSV = () => appFlow === 'Science Verification';
+
+  const setAppFlow = (_flow: AppFlowType) => {
+    // Disabled for now
+  };
+
+  return (
+    <AppFlowContext.Provider value={{ appFlow, isSV, setAppFlow }}>
+      {children}
+    </AppFlowContext.Provider>
+  );
+};
+
+export const useAppFlow = () => {
+  const context = useContext(AppFlowContext);
+  if (!context) throw new Error('useAppFlow must be used within AppFlowProvider');
+  return context;
+};
