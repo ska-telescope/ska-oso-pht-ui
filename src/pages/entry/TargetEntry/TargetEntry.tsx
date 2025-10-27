03e4edc5c2a503ebe2bea509a3946bec590d8321
Chloe <chloe.gallacher@cgi.com>
2025-10-27 13:37:22 +0000

    STAR-1744: Resolve unit test

--- src/pages/entry/TargetEntry/TargetEntry.tsx
+++ src/pages/entry/TargetEntry/TargetEntry.tsx
@@ -345,7 +345,7 @@ export default function TargetEntry({
         skyUnits={raType}
         value={dec}
         valueFocus={() => helpComponent(t('skyDirection.help.2.value'))}
-        isLow={getProposal()?.observations.some(obs => obs.observingBand === 0)}
+        isLow={getProposal()?.observations?.some(obs => obs.observingBand === 0)}
       />
     );
 
