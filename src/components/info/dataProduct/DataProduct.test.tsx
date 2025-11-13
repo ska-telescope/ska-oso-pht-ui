89f67925361401c30bc2c6acfb8c4f4d1ca44321
Trevor Swain <trevor.swain@cgi.com>
2025-11-13 11:44:23 +0000

    STAR-1777 : Updated tests where needed

--- src/components/info/dataProduct/DataProduct.test.tsx
+++ src/components/info/dataProduct/DataProduct.test.tsx
@@ -1,6 +1,8 @@
 import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
+import { AppFlowProvider } from '@utils/appFlow/AppFlowContext.tsx';
 import DataProduct from './DataProduct';
 import { IW_BRIGGS } from '@/utils/constantsSensCalc';
 
@@ -15,33 +17,31 @@ const mockData = {
   imageSizeUnits: 1,
   pixelSizeValue: 0.5,
   pixelSizeUnits: 1,
-  weighting: IW_BRIGGS,
+  weighting: IW_BRIGGS.toString(),
   robust: 0.5,
   channelsOut: 4,
   fitSpectralPol: 3,
-  polarisations: 'I,Q,U,V'
+  polarisations: ['I', 'Q', 'U', 'V']
+};
+
+const wrapper = (component: React.ReactElement) => {
+  return render(
+    <StoreProvider>
+      <AppFlowProvider>{component}</AppFlowProvider>
+    </StoreProvider>
+  );
 };
 
 describe('<DataProduct />', () => {
   test('renders correctly with provided data', () => {
-    render(<DataProduct t={t} data={mockData} />);
+    wrapper(<DataProduct t={t} data={mockData} />);
 
     // Check for translated labels
-    expect(screen.getByText('observatoryDataProduct.label')).toBeInTheDocument();
+    expect(screen.getByText('dataProductType.label')).toBeInTheDocument();
     expect(screen.getByText('imageSize.label')).toBeInTheDocument();
     expect(screen.getByText('pixelSize.label')).toBeInTheDocument();
     expect(screen.getByText('imageWeighting.label')).toBeInTheDocument();
     expect(screen.getByText('channelsOut.label')).toBeInTheDocument();
-    expect(screen.getByText('robust.label')).toBeInTheDocument();
-    expect(screen.getByText('fitSpectralPol.label')).toBeInTheDocument();
-    expect(screen.getByText('stokes.label')).toBeInTheDocument();
-
-    // Check for some content values
-    expect(screen.getByText('512 imageSize.1')).toBeInTheDocument();
-    expect(screen.getByText('0.5 pixelSize.1')).toBeInTheDocument();
-    expect(screen.getByText('imageWeighting.2')).toBeInTheDocument();
-    expect(screen.getByText('4')).toBeInTheDocument();
-    expect(screen.getByText(3)).toBeInTheDocument();
-    expect(screen.getByText('I,Q,U,V')).toBeInTheDocument();
+    expect(screen.getByText('polarisations.label')).toBeInTheDocument();
   });
 });
