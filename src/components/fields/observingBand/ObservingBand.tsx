3dabb75e5cfcb089943307f1af48530dbfc30365
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-11-17 22:43:21 +0000

    STAR-1755 - disable mid bands with osd data

--- src/components/fields/observingBand/ObservingBand.tsx
+++ src/components/fields/observingBand/ObservingBand.tsx
@@ -1,8 +1,15 @@
 import { DropDown } from '@ska-telescope/ska-gui-components';
 import { storageObject } from '@ska-telescope/ska-gui-local-storage';
 import { Grid } from '@mui/material';
-import { BANDWIDTH_TELESCOPE, LAB_IS_BOLD, LAB_POSITION } from '../../../utils/constants';
+import {
+  BANDWIDTH_TELESCOPE,
+  LAB_IS_BOLD,
+  LAB_POSITION,
+  TELESCOPE_LOW_NUM,
+  TELESCOPE_MID_NUM
+} from '../../../utils/constants';
 import { useScopedTranslation } from '@/services/i18n/useScopedTranslation';
+import { useOSDAccessors } from '@/utils/osd/useOSDAccessors/useOSDAccessors';
 
 interface ObservingBandFieldProps {
   disabled?: boolean;
@@ -26,14 +33,28 @@ export default function ObservingBandField({
   const { t } = useScopedTranslation();
   const { helpComponent } = storageObject.useStore();
   const FIELD = 'observingBand';
+  const { osdLOW } = useOSDAccessors();
+  const { osdMID } = useOSDAccessors();
 
-  const getOptions = () => BANDWIDTH_TELESCOPE;
+  const getOptions = () => {
+    let filteredOptions = BANDWIDTH_TELESCOPE; // TODO we should use observatoryConstants here
+
+    if (osdMID === null) {
+      filteredOptions = filteredOptions?.filter((e: any) => e.telescope === TELESCOPE_LOW_NUM);
+    }
+
+    if (osdLOW === null) {
+      filteredOptions = filteredOptions?.filter((e: any) => e.telescope === TELESCOPE_MID_NUM);
+    }
+
+    return filteredOptions;
+  };
 
   return (
     <Grid pt={1} spacing={0} container justifyContent="space-between" direction="row">
       <Grid pl={suffix ? 1 : 0} size={{ xs: suffix ? 12 - widthButton : 12 }}>
         <DropDown
-          disabled={disabled}
+          disabled={disabled || getOptions().length < 2}
           options={getOptions()}
           testId={FIELD}
           value={value}
