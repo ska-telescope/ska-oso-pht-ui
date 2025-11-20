b0d2efa8d867d15a3e90503761938acf85d1db5b
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-11-20 11:16:18 +0000

    STAR-1755 - fix mapping

--- src/utils/types/targetObservation.tsx
+++ src/utils/types/targetObservation.tsx
@@ -3,6 +3,7 @@ import { SensCalcResults } from '../../utils/types/sensCalcResults';
 type TargetObservation = {
   targetId: number;
   observationId: string;
+  dataProductsSDPId: string; // TODO make data product mandatory once used for sens calc results
   sensCalc: SensCalcResults;
 };
 
