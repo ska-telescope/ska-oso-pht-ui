8cb06c2a90b7724a442ecfcc525a452d8f9eb09b
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2025-12-09 13:27:36 +0000

    STAR-1818 - move more functionality into generate utility

--- src/pages/entry/TargetEntry/TargetEntry.tsx
+++ src/pages/entry/TargetEntry/TargetEntry.tsx
@@ -25,16 +25,8 @@ import {
 import { useNotify } from '@/utils/notify/useNotify';
 import { useScopedTranslation } from '@/services/i18n/useScopedTranslation';
 import { useAppFlow } from '@/utils/appFlow/AppFlowContext';
-import Observation from '@/utils/types/observation';
-import { calculateSensCalcData } from '@/utils/sensCalc/sensCalc';
-import { CalibrationStrategy } from '@/utils/types/calibrationStrategy';
-import { DataProductSDP } from '@/utils/types/dataProduct';
 import { useHelp } from '@/utils/help/useHelp';
-import {
-  calibrationOut,
-  dataProductSDPOut,
-  observationOut
-} from '@/utils/generateDefaultObservation/GenerateDefaultObservation';
+import { generateDefaults } from '@/utils/generateDefaultObservation/GenerateDefaultObservation';
 import { useOSDAccessors } from '@/utils/osd/useOSDAccessors/useOSDAccessors';
 interface TargetEntryProps {
   raType: number;
@@ -266,52 +258,23 @@ export default function TargetEntry({
         tiedArrayBeams: tiedArrayBeams ? (tiedArrayBeams as TiedArrayBeams) : null
       };
 
-      const getSensCalcData = async (observation: Observation, target: Target) => {
-        const response = await calculateSensCalcData(observation, target);
-        if (response) {
-          if (response.error) {
-            const errMsg = response.error;
-            notifyError(errMsg, NOTIFICATION_DELAY_IN_SECONDS);
-          }
-          return response;
+      const generateAuto = async () => {
+        const defaults = await generateDefaults(newTarget, getProposal, setProposal, autoLink);
+        if (defaults.success) {
+          notifySuccess(t('addTarget.success'), NOTIFICATION_DELAY_IN_SECONDS);
+        } else {
+          notifyError(defaults.error, NOTIFICATION_DELAY_IN_SECONDS);
         }
       };
 
       const addTargetAsync = async () => {
-        let newObservation = undefined;
-        let newCalibration = undefined;
-        let newDataProductSDP = undefined;
-        let sensCalcResult = undefined;
-        if (autoLink && typeof getProposal().scienceCategory === 'number') {
-          newObservation = observationOut(getProposal().scienceCategory as number);
-          newCalibration = calibrationOut(newObservation?.id);
-          newDataProductSDP = dataProductSDPOut(newObservation?.id, getProposal().scienceCategory);
-          sensCalcResult = await getSensCalcData(newObservation, newTarget);
+        if (autoLink) {
+          generateAuto();
+          return;
         }
         const updatedProposal = {
           ...getProposal(),
-          targets: [...(getProposal().targets ?? []), newTarget],
-          observations: autoLink
-            ? [newObservation].filter((obs): obs is Observation => obs !== undefined)
-            : getProposal().observations,
-          calibrationStrategy: autoLink
-            ? [...(getProposal().calibrationStrategy ?? []), newCalibration as CalibrationStrategy]
-            : getProposal().calibrationStrategy,
-          dataProductSDP: autoLink
-            ? [...(getProposal().dataProductSDP ?? []), newDataProductSDP as DataProductSDP]
-            : getProposal().dataProductSDP,
-          targetObservation: autoLink
-            ? sensCalcResult && newObservation && newObservation.id && newDataProductSDP?.id
-              ? [
-                  {
-                    targetId: newTarget?.id,
-                    observationId: newObservation?.id,
-                    dataProductsSDPId: newDataProductSDP.id,
-                    sensCalc: sensCalcResult
-                  }
-                ]
-              : []
-            : getProposal().targetObservation
+          targets: [...(getProposal().targets ?? []), newTarget]
         };
         setProposal(updatedProposal);
         notifySuccess(t('addTarget.success'), NOTIFICATION_DELAY_IN_SECONDS);
--- src/utils/generateDefaultObservation/GenerateDefaultObservation.tsx
+++ src/utils/generateDefaultObservation/GenerateDefaultObservation.tsx
@@ -1,8 +1,11 @@
 import { DEFAULT_DATA_PRODUCT, DEFAULT_OBSERVATIONS_LOW_AA2, TYPE_PST } from '../constants';
 import { generateId } from '../helpers';
+import { calculateSensCalcData } from '../sensCalc/sensCalc';
 import { CalibrationStrategy } from '../types/calibrationStrategy';
 import { DataProductSDP } from '../types/dataProduct';
 import Observation from '../types/observation';
+import { SensCalcResults } from '../types/sensCalcResults';
+import Target from '../types/target';
 
 export const observationOut = (obsMode: number) => {
   const defaultObs: Observation = {
@@ -34,4 +37,87 @@ export const dataProductSDPOut = (observationId: string, observationType: number
   };
 
   return newDSP;
+};
+
+const getSensCalcData = async (
+  observation: Observation,
+  target: Target
+): Promise<SensCalcResults | any> => {
+  const response = await calculateSensCalcData(observation, target);
+  if (response.error) {
+    const errMsg = response.error;
+    return errMsg;
+  }
+  return response;
+};
+
+const updateProposal = (
+  newTarget: Target,
+  newObservation: Observation,
+  newCalibration: CalibrationStrategy,
+  newDataProductSDP: DataProductSDP,
+  sensCalcResult: any,
+  autoLink: boolean,
+  getProposal: Function,
+  setProposal: Function
+) => {
+  const updatedProposal = {
+    ...getProposal(),
+    targets: [...(getProposal().targets ?? []), newTarget],
+    observations: autoLink
+      ? [newObservation].filter((obs): obs is Observation => obs !== undefined)
+      : getProposal().observations,
+    dataProductSDP: autoLink
+      ? [...(getProposal().dataProductSDP ?? []), newDataProductSDP as DataProductSDP]
+      : getProposal().dataProductSDP,
+    targetObservation: autoLink
+      ? sensCalcResult && newObservation && newObservation.id && newDataProductSDP?.id
+        ? [
+            {
+              targetId: newTarget?.id,
+              observationId: newObservation?.id,
+              dataProductsSDPId: newDataProductSDP.id,
+              sensCalc: sensCalcResult
+            }
+          ]
+        : []
+      : getProposal().targetObservation,
+    calibrationStrategy: autoLink
+      ? [...(getProposal().calibrationStrategy ?? []), newCalibration as CalibrationStrategy]
+      : getProposal().calibrationStrategy
+  };
+  setProposal(updatedProposal);
+};
+
+export const generateDefaults = async (
+  target: Target,
+  getProposal: Function,
+  setProposal: Function,
+  autoLink: boolean
+): Promise<any | void> => {
+  let newObservation = undefined;
+  let newDataProductSDP = undefined;
+  let sensCalcResult = undefined;
+  let newCalibration = undefined;
+  if (autoLink && typeof getProposal().scienceCategory === 'number') {
+    newObservation = observationOut(getProposal().scienceCategory as number);
+    newDataProductSDP = dataProductSDPOut(newObservation?.id, getProposal().scienceCategory);
+    sensCalcResult = await getSensCalcData(newObservation, target);
+    if (typeof sensCalcResult === 'string') {
+      return { success: false, error: sensCalcResult };
+    }
+    newCalibration = calibrationOut(newObservation?.id);
+
+    updateProposal(
+      target,
+      newObservation,
+      newCalibration,
+      newDataProductSDP,
+      sensCalcResult,
+      autoLink,
+      getProposal,
+      setProposal
+    );
+    return { success: true };
+  }
 };
