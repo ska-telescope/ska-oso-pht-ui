dd7e505e8ad3493120ae8216551cede59a781eb2
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2026-01-30 12:10:41 +0000

    STAR-1892 - fix sens calc url

--- charts/ska-oso-pht-ui/templates/_helpers.tpl
+++ charts/ska-oso-pht-ui/templates/_helpers.tpl
@@ -44,7 +44,7 @@ Create chart name and version as used by the chart label.
 {{- if .Values.runtimeEnv.skaSensitivityCalcUrl -}}
 {{ .Values.runtimeEnv.skaSensitivityCalcUrl }}
 {{- else -}}
-/{{ .Release.Namespace }}/api/v12/
+/{{ .Release.Namespace }}/api/v11/
 {{- end }}
 {{- end }}
 
--- setupTests.tsx
+++ setupTests.tsx
@@ -3,7 +3,7 @@ import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
 vi.stubEnv('BASE_URL', '/');
-vi.stubEnv('BACKEND_URL', 'ska-ost-senscalc/senscalc/api/v12');
+vi.stubEnv('BACKEND_URL', 'ska-ost-senscalc/senscalc/api/v11');
 vi.stubEnv('ENVJS_FILE', './public/env.js');
 
 afterEach(() => {
