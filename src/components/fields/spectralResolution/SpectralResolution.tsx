40b04fcdd67d0a4174b3d4d29e0d767d423d25a7
Chloe <chloe.gallacher@cgi.com>
2026-02-26 13:14:55 +0000

    STAR-1920: Resolve failing zoom calculations, update sens calc e2e test

--- src/components/fields/spectralResolution/SpectralResolution.tsx
+++ src/components/fields/spectralResolution/SpectralResolution.tsx
@@ -34,16 +34,9 @@ export default function SpectralResolutionField({
 
   const LOWContinuumBase = () => 5.43;
   const LOWZoomBase = () => {
-    const BASE_CLOCK_FREQUENCY = 781250;
-    const FRACTIONAL_RESAMPLING_RATIO = 32 / 27;
-    const FFT_SIZE_N = 4096;
-    const DECIMATION_FACTOR = 16;
-    const CLOCKING_COMPONENT = BASE_CLOCK_FREQUENCY * FRACTIONAL_RESAMPLING_RATIO;
-    const RESOLUTION_COMPONENT = FFT_SIZE_N / DECIMATION_FACTOR;
-    const BASE_SPECTRAL_RESOLUTION_HZ = CLOCKING_COMPONENT / RESOLUTION_COMPONENT;
-
     const powersTwo = [1, 2, 4, 8, 16, 32, 64, 128];
-    const results = powersTwo.map(obj => obj * BASE_SPECTRAL_RESOLUTION_HZ);
+    const baseSpectralResolutionHz = (781250 * 32) / 27 / 4096 / 16;
+    const results = powersTwo.map(obj => obj * baseSpectralResolutionHz);
     return (results[bandWidth - 1]?.toFixed(2) as unknown) as number;
   };
 
