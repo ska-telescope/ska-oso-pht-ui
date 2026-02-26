ba4ebd2c39cc8d2e2f45331793d56c8daa16b9e8
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2026-02-26 10:28:16 +0000

    STAR-1877 - wip

--- src/services/axios/use/useOSDAPI/useOSDAPI.tsx
+++ src/services/axios/use/useOSDAPI/useOSDAPI.tsx
@@ -4,6 +4,7 @@ import useAxiosAuthClient from '../../axiosAuthClient/axiosAuthClient';
 import GetObservatoryData from '../../get/getObservatoryData/getObservatoryData';
 import ObservatoryData from '@/utils/types/observatoryData';
 import { SV_LOW_AA2_CYCLE_NUMBER } from '@/utils/constants';
+import GetOSDCycles from '../../get/getObservatoryData/getOSDCycles';
 
 export const useOSDAPI = (setAxiosError: (error: string) => void) => {
   const { application, updateAppContent3 } = storageObject.useStore();
@@ -28,7 +29,8 @@ export const useOSDAPI = (setAxiosError: (error: string) => void) => {
 
     const fetchObservatoryData = async () => {
       try {
-        const response = await GetObservatoryData(authClient, SV_LOW_AA2_CYCLE_NUMBER);
+        // const response = await GetObservatoryData(authClient, SV_LOW_AA2_CYCLE_NUMBER);
+        const response = await GetOSDCycles(authClient);
         if (typeof response === 'string' || (response && (response as any).error)) {
           setAxiosError(response.toString());
         } else if (isObservatoryData(response)) {
