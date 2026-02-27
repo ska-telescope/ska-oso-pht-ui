fe01ff07f56bdf104fb86fc96141199273afce83
Chloe <chloe.gallacher@cgi.com>
2026-02-27 09:42:55 +0000

    STAR-1922: Add guest e2e test, testing link to sensitivity calculator

--- tests/cypress/e2e/users/guest/guest.test.js
+++ tests/cypress/e2e/users/guest/guest.test.js
@@ -1,4 +1,9 @@
-import { initializeUserNotLoggedIn, clearLocalStorage, verify } from '../../common/common';
+import {
+  initializeUserNotLoggedIn,
+  clearLocalStorage,
+  verify,
+  verifyInformationBannerText
+} from '../../common/common';
 
 describe('Guest User', () => {
   beforeEach(() => {
@@ -11,5 +16,23 @@ describe('Guest User', () => {
 
   it('Validate user has not yet signed in, log in button is present', () => {
     verify('loginButton');
+  });
+
+  it('Verify a guest user can use the external link to the sensitivity calculator application', () => {
+    //verify not logged in information banner
+    verifyInformationBannerText('NOT LOGGED IN, NO SUBMISSIONS AVAILABLE');
+
+    cy.window().then(win => {
+      cy.stub(win, 'open').as('windowOpen');
+    });
+
+    cy.get('[data-testId="SensCalcButtonTestId"]').click();
+
+    cy.get('@windowOpen').should('have.been.called');
+
+    cy.get('@windowOpen').should(
+      'have.been.calledWith',
+      'https://sensitivity-calculator.skao.int/'
+    );
   });
 });
