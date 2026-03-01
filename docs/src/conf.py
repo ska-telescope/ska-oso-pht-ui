670406325347e7d7fc80b3b93c100124f62fb09d
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2026-03-01 15:34:03 +0000

    REL-2756 - bump version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, Stargazer'
 
 # The short X.Y version
-version = '10.0.0'
+version = '10.1.0'
 # The full version, including alpha/beta/rc tags
-release = '10.0.0'
+release = '10.1.0'
 
 
 rst_prolog = r"""
