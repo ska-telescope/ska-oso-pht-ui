e4f6b05978daa20828350902e36a6727ed301de2
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2026-06-15 10:50:39 +0100

    BTN-3415 first pass at using the correct response from the coordinate endpoint. Removing unecessary mappers

--- src/services/axios/get/getCoordinates/getCoordinates.tsx
+++ src/services/axios/get/getCoordinates/getCoordinates.tsx
@@ -1,16 +1,16 @@
-import { SKA_OSO_SERVICES_URL, USE_LOCAL_DATA } from '@utils/constants.ts';
+import { SKA_OSO_SERVICES_URL} from '@utils/constants.ts';
 import axiosClient from '../../axiosClient/axiosClient.tsx';
 
-const MOCK_UNITS = ['equatorial', 'galactic'];
+const UNITS = ['equatorial', 'galactic'];
 
 async function GetCoordinates(targetName: string, skyUnits: number) {
-  const units = skyUnits < 0 || skyUnits > MOCK_UNITS.length - 1 ? 0 : skyUnits;
+  const units = skyUnits < 0 || skyUnits > UNITS.length - 1 ? 0 : skyUnits;
 
   try {
     const URL_PATH = `/coordinates/`;
     /* This is a rare case where the OSO-services endpoint is open to the public */
     const result = await axiosClient.get(
-      `${SKA_OSO_SERVICES_URL}${URL_PATH}${targetName}/${MOCK_UNITS[units]}`
+      `${SKA_OSO_SERVICES_URL}${URL_PATH}${targetName}/${UNITS[units]}`
     );
     return typeof result === 'undefined' ? 'error.API_UNKNOWN_ERROR' : result.data;
   } catch (e) {
