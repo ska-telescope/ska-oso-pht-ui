309662533d13eace91d1b22f5cead19a2ccf7914
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2026-06-15 14:53:25 +0200

    SDR-1708 remove db test before deleting db

--- scripts/test-aws-prod.sh
+++ scripts/test-aws-prod.sh
@@ -1,39 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-kubectl apply -f - <<'EOF'
-apiVersion: v1
-kind: Pod
-metadata:
-  name: pg-pht-test-client
-  namespace: prod-ska-oso-pht-ui
-spec:
-  containers:
-  - name: psql
-    image: postgres:18
-    command: ["psql", "-l"]
-    env:
-    - name: PGHOST
-      value: "pg-pht-test.prod-ska-oso-pht-ui.svc.cluster.local"
-    - name: PGPORT
-      value: "5432"
-    - name: PGUSER
-      valueFrom:
-        secretKeyRef:
-          name: pg-pht-test
-          key: superuser-username
-    - name: PGPASSWORD
-      valueFrom:
-        secretKeyRef:
-          name: pg-pht-test
-          key: superuser-password
-  restartPolicy: Never
-EOF
-
-kubectl wait --for=jsonpath='{.status.phase}'=Succeeded pod/pg-pht-test-client -n prod-ska-oso-pht-ui --timeout=60s
-kubectl logs pg-pht-test-client -n prod-ska-oso-pht-ui
-kubectl delete pod pg-pht-test-client -n prod-ska-oso-pht-ui
-
 kubectl create serviceaccount ska-oso-pht-ui -n prod-ska-oso-pht-ui --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl apply -f - <<'EOF'
