19088d2b7799e6f5347bafd5128e3b98eceae98e
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2026-07-01 15:48:10 +0100

    BTN-3268 creating new dropdown component for SolarSystem objects

---
+++ src/components/fields/solarSystemObject/solarSystemObject.tsx
@@ -0,0 +1,45 @@
+import { DropDown } from '@ska-telescope/ska-gui-components';
+import { Box } from '@mui/material';
+import { useScopedTranslation } from '@/services/i18n/useScopedTranslation';
+import React from 'react';
+import { SSO_OPTIONS } from '@utils/constants.ts';
+
+interface SolarSystemObjectFieldProps {
+  setValue?: Function,
+  value: string,
+  valueFocus?: Function,
+  disabled?: boolean
+}
+
+
+export default function SolarSystemObjectField({
+                                                    setValue,
+                                                    value,
+                                                    valueFocus,
+                                                  }: SolarSystemObjectFieldProps) {
+  const { t } = useScopedTranslation();
+  const FIELD = 'solarSystemObject';
+
+  const SolarSystemObjectValueField = () => {
+
+    return (
+      <Box pt={1}>
+        <DropDown
+          options={SSO_OPTIONS}
+          required
+          label={t('name.label')}
+          testId={FIELD + 'Type'}
+          value={value}
+          setValue={setValue}
+          onFocus={valueFocus}
+        />
+      </Box>
+    );
+  };
+
+  return (
+    <Box pb={0} pt={0} sx={{ width: '100%' }}>
+      {SolarSystemObjectValueField()}
+    </Box>
+  );
+}
