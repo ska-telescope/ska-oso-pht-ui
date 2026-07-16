ab4b1781a1719a92a14dd3fc2dd47905f21fa875
Thad Kenny <thaddeus.kenny@stfc.ac.uk>
2026-07-16 11:30:14 +0100

    BTN-3245 handle breaking change in PDM for visibilities data product params

--- src/services/axios/get/getProposal/getProposal.tsx
+++ src/services/axios/get/getProposal/getProposal.tsx
@@ -307,10 +307,10 @@ const getDataProductSDP = (inValue: DataProductSDPsBackend[] | null): DataProduc
           polarisations: 'polarisations' in script ? script.polarisations : undefined,
           channelsOut: 'channels_out' in script ? (Number(script.channels_out) ?? 0) : 0,
           taperValue: 'gaussian_taper' in script ? (Number(script.gaussian_taper) ?? 0) : 0,
-          timeAveraging:
-            'time_averaging' in script ? (Number(script.time_averaging.value) ?? 0) : 0,
-          frequencyAveraging:
-            'frequency_averaging' in script ? (Number(script.frequency_averaging.value) ?? 0) : 0,
+          // TODO - we shouldn't need so many conditionals in this function. Fix when we have
+          // refactored with better typing
+          timeAveraging: script.time_averaging ?? 1,
+          frequencyAveraging: script.frequency_averaging ?? 1,
           bitDepth: 'bit_depth' in script ? (Number(script.bit_depth) ?? 1) : 1,
           continuumSubtraction:
             'continuum_subtraction' in script ? Boolean(script.continuum_subtraction) : false,
