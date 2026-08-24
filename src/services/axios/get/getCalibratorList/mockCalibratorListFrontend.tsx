4976172d324072d475da7eb4b58734955c69a886
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2026-08-24 13:34:54 +0100

    BTN-3421 updating one of the mocks kicking about

--- src/services/axios/get/getCalibratorList/mockCalibratorListFrontend.tsx
+++ src/services/axios/get/getCalibratorList/mockCalibratorListFrontend.tsx
@@ -2,10 +2,21 @@ import { Calibrator } from '@/utils/types/calibrationStrategy';
 
 export const MockCalibratorFrontendList: Calibrator[] = [
   {
-    calibrationIntent: 'Flux',
-    name: 'PKS 1934-638',
-    durationMin: 10,
-    choice: 'Highest elevation',
-    notes: 'Flux/BP on PKS 1934-638, 15-min phase cycle.'
+    "calibrationIntent": "flux",
+    "durationSeconds": 600,
+    "name": "3C 444",
+    "relativeToScan": "before_each_scan",
+    "selectionStrategy": "highest_elevation",
+    "targetId": "calibrator-00001",
+    "notes": null,
+  },
+    {
+    "calibrationIntent": "flux",
+    "durationSeconds": 600,
+    "name": "3C 444",
+    "relativeToScan": "after_each_scan",
+    "selectionStrategy": "highest_elevation",
+    "targetId": "calibrator-00001",
+    "notes": null,
   }
 ];
